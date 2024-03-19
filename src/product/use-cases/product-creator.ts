8a757ea83f7b389418e4d0f75c50e1efb257f49c
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-19 07:39:56 -0500

    Implement new product creation use-case
    Introduced a new use case in the product domain for creating new products. This use-case validates the incoming product data against the set schema, checks for existing products with the same SKU and if none found, calls upon the repository to create the product object.

---
+++ src/product/use-cases/product-creator.ts
@@ -0,0 +1,28 @@
+import { Product, ProductSearchParams } from "@/product/types";
+import { ProductSchema } from "@/product/schema";
+import { response } from "@/lib/types";
+
+interface Repository {
+  create: (product: Product) => Promise<response<Product>>;
+  findBy: (params: ProductSearchParams) => Promise<response<Product>>;
+}
+
+export default async function productCreator(
+  repository: Repository,
+  product: Product,
+): Promise<response<Product>> {
+  const parsedProduct = ProductSchema.safeParse(product);
+  if (!parsedProduct.success) {
+    return { success: false, message: parsedProduct.error.message };
+  }
+
+  const { success: productFound } = await repository.findBy({
+    sku: product.sku,
+  });
+
+  if (productFound) {
+    return { success: false, message: "Ya existe un producto con el sku" };
+  }
+
+  return repository.create(product);
+}
