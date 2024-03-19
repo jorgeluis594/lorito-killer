ebf2815d30c46a2bf255462186b8563b8509ec58
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-18 21:32:31 -0500

    Add Category database repository

---
+++ src/category/db_respository.ts
@@ -0,0 +1,39 @@
+import prisma from "@/lib/prisma";
+import { Category } from "./types";
+import { response } from "@/lib/types"
+
+export const create = async (category: Category):Promise<response<Category>> => {
+  try {
+    const foundResponse = await findByName(category.name)
+    if (foundResponse.success) {
+      return foundResponse as response
+    }
+
+    const createdCategory = await prisma.category.create({ data: category })
+    return { success: true, data: createdCategory } as response<Category>
+  } catch (error: any) {
+    return { success: false, message: error.message } as response
+  }
+}
+
+const findByName = async (name: string):Promise<response<Category>> => {
+  try {
+    const category = await prisma.category.findMany({
+      where: {
+        name: {
+          equals: name,
+          mode: "insensitive"
+        }
+      },
+      take: 1
+    })
+
+    if (category.length) {
+      return { success: true, data: category[0] } as response<Category>
+    } else {
+      return { success: false, message: "Category not found" } as response
+    }
+  } catch (error: any) {
+    return { success: false, message: error.message } as response
+  }
+}
