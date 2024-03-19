0dc03be930333caeb2dd28e629db95d104fe8a8e
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-19 07:45:03 -0500

    Implement revalidation in product routes

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,23 +1,47 @@
-import { find as findProduct, update as UpdateProduct, deleteProduct } from "@/product/db_repository";
+import {
+  find as findProduct,
+  update as UpdateProduct,
+  deleteProduct,
+} from "@/product/db_repository";
 import { Product } from "@/product/types";
-import {NextResponse} from "next/server";
-export async function PUT(req: Request, { params }: { params: {id: string} }) {
-  const productData = await req.json() as Product
-  const {success} = await findProduct(params.id)
+import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const productData = (await req.json()) as Product;
+  const { success } = await findProduct(params.id);
   if (!success) {
-    return NextResponse.json({ success: false, message: "Product not found" }, { status: 404 })
+    return NextResponse.json(
+      { success: false, message: "Product not found" },
+      { status: 404 },
+    );
   }
 
-  const response = await UpdateProduct(productData)
-  return NextResponse.json(response, { status: response.success ? 200 : 400 })
+  const response = await UpdateProduct(productData);
+  if (response.success) {
+    revalidatePath("/products");
+  }
+  return NextResponse.json(response, { status: response.success ? 200 : 400 });
 }
 
-export async function DELETE(_req: Request, { params }: { params: {id: string} }) {
-  const {success, data: product } = await findProduct(params.id)
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { id: string } },
+) {
+  const { success, data: product } = await findProduct(params.id);
   if (!success || !product) {
-    return NextResponse.json({ success: false, message: "Product not found" }, { status: 404 })
+    return NextResponse.json(
+      { success: false, message: "Product not found" },
+      { status: 404 },
+    );
   }
 
-  const response = await deleteProduct(product)
-  return NextResponse.json(response, { status: response.success ? 200 : 400 })
+  const response = await deleteProduct(product);
+  if (response.success) {
+    revalidatePath("/products");
+  }
+
+  return NextResponse.json(response, { status: response.success ? 200 : 400 });
 }
