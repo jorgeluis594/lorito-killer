ad1bd2a478c9954f6fad1cefd7c490aaec3dd23d
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-24 01:05:04 -0500

    Add Suspense wrapper to the Products component for error handling

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,20 @@
 import BreadCrumb from "@/components/breadcrumb";
 import ProductsClient from "@/components/tables/products/client";
 import { getMany as getManyProducts } from "@/product/db_repository";
+import { Suspense } from "react";
 
 const breadcrumbItems = [{ title: "Productos", link: "/products" }];
+
+const Products = async () => {
+  const response = await getManyProducts();
+
+  if (!response.success) {
+    return <div>Error: {response.message}</div>;
+  }
+
+  return <ProductsClient data={response.data} />;
+};
+
 export default async function Page() {
   const response = await getManyProducts();
 
@@ -10,7 +22,9 @@ export default async function Page() {
     <>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
         <BreadCrumb items={breadcrumbItems} />
-        <ProductsClient data={response.data || []} />
+        <Suspense fallback={<p>Loading</p>}>
+          <Products />
+        </Suspense>
       </div>
     </>
   );
