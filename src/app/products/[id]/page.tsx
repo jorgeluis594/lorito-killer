86de7d3afbc3e7d3f0b460300acc4618c11924d3
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-24 18:00:17 -0500

    Refactor product forms for better error handling

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,14 +3,13 @@ import { ProductForm } from "@/components/forms/product-form/product-form";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import React from "react";
 import { find as findProduct } from "@/product/db_repository";
-import { notFound } from 'next/navigation'
-import {getMany} from "@/category/db_respository";
-import {Category} from "@/category/types";
+import { notFound } from "next/navigation";
+import { getMany } from "@/category/db_respository";
 
-export default async function Page({params}: { params: {id: string} }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
 
-  const response = await findProduct(id)
+  const response = await findProduct(id);
   if (!response.success) {
     return notFound();
   }
@@ -20,12 +19,17 @@ export default async function Page({params}: { params: {id: string} }) {
     { title: "Editar", link: `/products/${id}` },
   ];
 
+  const categoriesResponse = await getMany();
+  if (!categoriesResponse.success) {
+    return notFound();
+  }
+
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-5">
         <BreadCrumb items={breadcrumbItems} />
         <ProductForm
-          categories={(await getMany()).data as Category[]}
+          categories={categoriesResponse.data}
           initialProduct={response.data}
         />
       </div>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,9 @@
 import BreadCrumb from "@/components/breadcrumb";
-import { getMany as getManyProducts } from "@/product/db_repository";
 import ListProducts from "@/components/product/list-product";
 
 const breadcrumbItems = [{ title: "Productos", link: "/products" }];
 
 export default async function Page() {
-  const response = await getManyProducts();
-
   return (
     <>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
