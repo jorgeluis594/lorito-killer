924e76aa8cfff1795a4316601a6272ea49dbd9f7
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-26 02:44:45 -0500

    Add ProductItem component to order form

---
+++ src/components/forms/order-form/product-item.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Product } from "@/product/types";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { useOrderFormStore } from "@/components/forms/order-form/order-form-provider";
+
+export default function ProductItem({ product }: { product: Product }) {
+  const photoUrl = product.photos![0]?.url || "";
+  const { addProduct } = useOrderFormStore((state) => state);
+
+  const onAddProductToCart = () => {
+    addProduct(product);
+  };
+
+  return (
+    <Card onClick={onAddProductToCart}>
+      <CardContent className="px-4">
+        <div className="mt-2 mx-auto relative w-[100px] h-[100px] rounded-md overflow-hidden">
+          <Image fill className="object-cover" alt="Image" src={photoUrl} />
+        </div>
+        <p className="text-sm text-center font-light leading-none mt-5">
+          {product.name}
+        </p>
+        <p className="text-lg text-center font-semibold mt-4">
+          {product.price}
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
--- src/components/forms/order-form/product-list.tsx
+++ src/components/forms/order-form/product-list.tsx
@@ -1,31 +1,8 @@
-"use client";
-
 import { Product } from "@/product/types";
-import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
+import ProductItem from "./product-item";
 
 interface ProductListProps {
   products: Product[];
-}
-
-function ProductItem({ product }: { product: Product }) {
-  const photoUrl = product.photos![0]?.url || "";
-
-  return (
-    <Card>
-      <CardContent className="px-4">
-        <div className="mt-2 mx-auto relative w-[100px] h-[100px] rounded-md overflow-hidden">
-          <Image fill className="object-cover" alt="Image" src={photoUrl} />
-        </div>
-        <p className="text-sm text-center font-light leading-none mt-5">
-          {product.name}
-        </p>
-        <p className="text-lg text-center font-semibold mt-4">
-          {product.price}
-        </p>
-      </CardContent>
-    </Card>
-  );
 }
 
 export default function ProductList({ products }: ProductListProps) {
