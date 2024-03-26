ee30232b717c3ec5a7cd6a7c61297e8ff7e4e712
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-26 01:58:22 -0500

    Improve product display in order form

--- src/components/forms/order-form/product-list.tsx
+++ src/components/forms/order-form/product-list.tsx
@@ -1,15 +1,30 @@
+"use client";
+
 import { Product } from "@/product/types";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
 interface ProductListProps {
   products: Product[];
 }
 
 function ProductItem({ product }: { product: Product }) {
+  const photoUrl = product.photos![0]?.url || "";
+
   return (
-    <div className="border p-2">
-      <div className="text-center">{product.name}</div>
-      <div className="text-center">{product.price}</div>
-    </div>
+    <Card>
+      <CardContent className="px-4">
+        <div className="mt-2 mx-auto relative w-[100px] h-[100px] rounded-md overflow-hidden">
+          <Image fill className="object-cover" alt="Image" src={photoUrl} />
+        </div>
+        <p className="text-sm text-center font-light leading-none mt-5">
+          {product.name}
+        </p>
+        <p className="text-lg text-center font-semibold mt-4">
+          {product.price}
+        </p>
+      </CardContent>
+    </Card>
   );
 }
 
