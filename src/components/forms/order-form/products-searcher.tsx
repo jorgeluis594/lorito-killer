cecdfd85a944cef98e1a15da7b5cc7e898ae8c08
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-30 21:37:15 -0500

    Add debounce function and integrate into product search

--- src/components/forms/order-form/products-searcher.tsx
+++ src/components/forms/order-form/products-searcher.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
@@ -9,6 +9,7 @@ import { response } from "@/lib/types";
 import { search as searchProducts, getMany } from "@/product/api_repository";
 import { useToast } from "@/components/ui/use-toast";
 import ProductList from "@/components/forms/order-form/product-list";
+import { debounce } from "@/lib/utils";
 
 export default function ProductsSearcher() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -35,6 +36,15 @@ export default function ProductsSearcher() {
     }
   }
 
+  const onSearchChange = debounce(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setSearch(value);
+      await onSearchSubmit();
+    },
+    100,
+  );
+
   useEffect(() => {
     onSearchSubmit();
   }, []);
@@ -45,10 +55,7 @@ export default function ProductsSearcher() {
         <Button type="button" onClick={onSearchSubmit}>
           <Search className="h-4 w-5" />
         </Button>
-        <Input
-          placeholder="Nombre del producto"
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <Input placeholder="Nombre del producto" onChange={onSearchChange} />
       </div>
 
       <ProductList products={products} />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,18 @@ export function cn(...inputs: ClassValue[]) {
 export const formatPrice = (price: number) => {
   return formater.format(price);
 };
+
+export const debounce = <F extends (...args: any[]) => any>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: NodeJS.Timeout;
+
+  return (...args: Parameters<F>): Promise<ReturnType<F>> =>
+    new Promise((resolve) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        resolve(func(...args));
+      }, waitFor);
+    });
+};
