72bde529037f23731a7b09c44d3cc6a1e0c62c9e
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-30 19:00:53 -0500

    Add product, manage quantities, and reset order functionality added in order-form-provider.
    The changes introduced methods to add a product to an order, increase or decrease the product quantity, and reset an order. These functionalities were moved from the OrderFormStore, thereby simplifying the store and allowing for more straightforward management of orders.

--- src/components/forms/order-form/order-form-provider.tsx
+++ src/components/forms/order-form/order-form-provider.tsx
@@ -7,7 +7,9 @@ import {
   type OrderFormStore,
   createOrderFormStore,
   initOrderFormStore,
+  Actions,
 } from "./store";
+import { Product } from "@/product/types";
 
 const OrderFormStoreContext = createContext<StoreApi<OrderFormStore> | null>(
   null,
@@ -42,4 +44,91 @@ export const useOrderFormStore = <T,>(
   }
 
   return useStore(orderFormStoreContext, selector);
+};
+
+export const useOrderFormActions = (): Actions => {
+  const orderFormStoreContext = useContext(OrderFormStoreContext);
+  if (!orderFormStoreContext) {
+    throw new Error(
+      "useOrderFormStore must be used within a OrderFormProvider",
+    );
+  }
+
+  return {
+    addProduct: (product: Product) => {
+      orderFormStoreContext.setState((state) => {
+        const orderItem = state.orderItems.find(
+          (item) => item.product.id === product.id,
+        );
+
+        if (orderItem) {
+          orderItem.quantity += 1;
+        } else {
+          state.orderItems.push({
+            product,
+            id: crypto.randomUUID(),
+            quantity: 1,
+            total: product.price,
+          });
+        }
+        state.total += product.price;
+
+        return { ...state, orderItems: [...state.orderItems] };
+      });
+    },
+    reset: () => {
+      const order = initOrderFormStore();
+      console.log({ order });
+      console.log({ order });
+      console.log({ order });
+      console.log({ order });
+      orderFormStoreContext.setState({
+        ...initOrderFormStore(),
+        orderItems: [],
+      });
+    },
+    increaseQuantity: (orderItemId: string) => {
+      orderFormStoreContext.setState((state) => {
+        const orderItem = state.orderItems.find(
+          (item) => item.id === orderItemId,
+        );
+
+        if (!orderItem) {
+          console.error("Order item not found");
+        } else if (orderItem.quantity >= orderItem.product.stock) {
+          console.error("Product stock exceeded");
+        } else {
+          orderItem.quantity += 1;
+          orderItem.total = orderItem.product.price * orderItem.quantity;
+          state.total += orderItem.product.price;
+        }
+
+        return { ...state, orderItems: [...state.orderItems] };
+      });
+    },
+    decreaseQuantity: (orderItemId: string) => {
+      orderFormStoreContext.setState((state) => {
+        const orderItem = state.orderItems.find(
+          (item) => item.id === orderItemId,
+        );
+
+        if (!orderItem) {
+          console.error("Order item not found");
+        } else if (orderItem.quantity <= 0) {
+          console.error("Product quantity can't be less than 1");
+        } else if (orderItem.quantity == 1) {
+          state.orderItems = state.orderItems.filter(
+            (item) => item.id !== orderItemId,
+          );
+          state.total -= orderItem.product.price;
+        } else {
+          orderItem.quantity -= 1;
+          orderItem.total = orderItem.product.price * orderItem.quantity;
+          state.total -= orderItem.product.price;
+        }
+
+        return { ...state, orderItems: [...state.orderItems] };
+      });
+    },
+  };
 };
--- src/components/forms/order-form/store.ts
+++ src/components/forms/order-form/store.ts
@@ -3,13 +3,14 @@ import { createStore } from "zustand/vanilla";
 import { Order } from "@/order/types";
 import { Product } from "@/product/types";
 
-type Actions = {
+export type Actions = {
   addProduct: (product: Product) => void;
+  reset: () => void;
   increaseQuantity: (productId: string) => void;
   decreaseQuantity: (productId: string) => void;
 };
 
-export type OrderFormStore = Order & Actions;
+export type OrderFormStore = Order;
 
 const defaultInitState: Order = {
   orderItems: [],
@@ -18,79 +19,11 @@ const defaultInitState: Order = {
 };
 
 export const initOrderFormStore = (): Order => {
-  return {
-    orderItems: [],
-    status: "pending",
-    total: 0,
-  };
+  return { ...defaultInitState };
 };
 
 export const createOrderFormStore = (initState: Order = defaultInitState) => {
-  return createStore<OrderFormStore>()((set) => ({
+  return createStore<OrderFormStore>()(() => ({
     ...initState,
-    addProduct: (product: Product) => {
-      set((state) => {
-        const orderItem = state.orderItems.find(
-          (item) => item.product.id === product.id,
-        );
-
-        if (orderItem) {
-          orderItem.quantity += 1;
-        } else {
-          state.orderItems.push({
-            product,
-            id: crypto.randomUUID(),
-            quantity: 1,
-            total: product.price,
-          });
-        }
-        state.total += product.price;
-
-        return { ...state, orderItems: [...state.orderItems] };
-      });
-    },
-    increaseQuantity: (orderItemId: string) => {
-      set((state) => {
-        const orderItem = state.orderItems.find(
-          (item) => item.id === orderItemId,
-        );
-
-        if (!orderItem) {
-          console.error("Order item not found");
-        } else if (orderItem.quantity >= orderItem.product.stock) {
-          console.error("Product stock exceeded");
-        } else {
-          orderItem.quantity += 1;
-          orderItem.total = orderItem.product.price * orderItem.quantity;
-          state.total += orderItem.product.price;
-        }
-
-        return { ...state, orderItems: [...state.orderItems] };
-      });
-    },
-    decreaseQuantity: (orderItemId: string) => {
-      set((state) => {
-        const orderItem = state.orderItems.find(
-          (item) => item.id === orderItemId,
-        );
-
-        if (!orderItem) {
-          console.error("Order item not found");
-        } else if (orderItem.quantity <= 0) {
-          console.error("Product quantity can't be less than 1");
-        } else if (orderItem.quantity == 1) {
-          state.orderItems = state.orderItems.filter(
-            (item) => item.id !== orderItemId,
-          );
-          state.total -= orderItem.product.price;
-        } else {
-          orderItem.quantity -= 1;
-          orderItem.total = orderItem.product.price * orderItem.quantity;
-          state.total -= orderItem.product.price;
-        }
-
-        return { ...state, orderItems: [...state.orderItems] };
-      });
-    },
   }));
 };
