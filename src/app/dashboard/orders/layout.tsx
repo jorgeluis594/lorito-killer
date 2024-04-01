f35dec9c3f548c829509242f197dcfe391b66737
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-01 06:59:06 -0500

    Refactor file structure for `dashboard` module and add new `OrdersLayout` file.

---
+++ src/app/dashboard/orders/layout.tsx
@@ -0,0 +1,40 @@
+import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { OrderFormProvider } from "@/components/forms/order-form/order-form-provider";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { MobileSidebar } from "@/components/layout/mobile-sidebar";
+import { UserNav } from "@/components/layout/user-nav";
+
+export default function OrdersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <OrderFormProvider>
+        <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
+          <nav className="h-14 flex items-center justify-between px-4">
+            <div className={cn("block")}>
+              <MobileSidebar />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <UserNav />
+            </div>
+          </nav>
+        </div>
+        <div className="flex h-screen overflow-hidden">
+          <main className="w-full pt-14">
+            <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+            {children}
+          </main>
+        </div>
+      </OrderFormProvider>
+    </>
+  );
+}
