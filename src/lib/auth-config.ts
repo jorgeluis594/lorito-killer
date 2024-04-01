f05a106eedecb15937a72a79a25520659c16c36a
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-31 22:05:14 -0500

    Revamp auth configuration in NextAuth

--- src/lib/auth-config.ts
+++ src/lib/auth-config.ts
@@ -1,21 +1,43 @@
-import type { NextAuthConfig } from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import CreateUserSchema from "@/user/schemas/create-user-schema";
+import { getUserByEmail } from "@/user/db_repository";
+import bcrypt from "bcrypt";
 
-export const authConfig = {
+export const authConfig: NextAuthOptions = {
   pages: {
     signIn: "/login",
   },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnSignIn = nextUrl.pathname.startsWith("/login");
-      if (isOnSignIn) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-      return true;
-    },
-  },
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "ejemplo@gmail.com",
+        },
+        password: { label: "Contraseña", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const parsedCredentials = CreateUserSchema.safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+        const userResponse = await getUserByEmail(email);
+        if (!userResponse.success) return null;
+        const isValidPassword = await bcrypt.compare(
+          password,
+          userResponse.data.password,
+        );
+        if (!isValidPassword) return null;
+
+        const { password: _, ...user } = userResponse.data;
+        console.log({ user });
+        return { ...user, id: user.id! };
+      },
+    }),
+  ],
+};
