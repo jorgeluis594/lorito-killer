5d50437dc2fcf9b288460a11a99f7eefb53540b9
Jorge Gonzalez <jorg3.594@gmail.com>
2024-03-31 22:09:28 -0500

    Move page.tsx to dashboard directory and update title

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -16,9 +16,7 @@ export default function page() {
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Pollo gay
-          </h2>
+          <h2 className="text-3xl font-bold tracking-tight">Datos</h2>
         </div>
         <Tabs defaultValue="overview" className="space-y-4">
           <TabsList>
