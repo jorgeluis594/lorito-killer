0f6baebcc09ae55922d460478dc4611faa889480
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-01 13:24:20 -0500

    Add revalidation of path after order creation

--- src/order/actions.ts
+++ src/order/actions.ts
@@ -3,7 +3,13 @@
 import { Order } from "./types";
 import { response } from "@/lib/types";
 import { create as createOrder } from "./db_repository";
+import { revalidatePath } from "next/cache";
 
 export const create = async (data: Order): Promise<response<Order>> => {
-  return await createOrder(data);
+  const createOrderResponse = await createOrder(data);
+  if (createOrderResponse.success) {
+    revalidatePath("/api/orders");
+  }
+
+  return createOrderResponse;
 };
