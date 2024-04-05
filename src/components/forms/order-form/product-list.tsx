2cf62303144ecf144e4da38ee2a1d44d433f4eaf
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-05 16:31:19 -0500

    Update grid styling in ProductList component
    Modified the styling properties of the ProductList component. This was achieved by adjusting padding and grid column width to improve layout and responsiveness. This change provides a more consistent display across different screen sizes.

--- src/components/forms/order-form/product-list.tsx
+++ src/components/forms/order-form/product-list.tsx
@@ -7,7 +7,7 @@ interface ProductListProps {
 
 export default function ProductList({ products }: ProductListProps) {
   return (
-    <div className="p-4 mt-4 flex-wrap justify-center grid grid-flow-row auto-rows-[250px] grid-cols-[repeat(auto-fill,220px)] gap-4">
+    <div className="py-4 h-full flex-wrap grid grid-flow-row auto-rows-[250px] grid-cols-[repeat(auto-fill,200px)] gap-4">
       {products.length ? (
         products.map((product) => (
           <ProductItem key={product.id} product={product} />
