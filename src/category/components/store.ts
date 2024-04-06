456474ff150a70cef3d6ea63743ad7b0cff67127
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-06 11:11:31 -0500

    Add sorting mechanism to category store
    A sorting function has been implemented to the category store. This function sorts categories in alphabetical order and applies this sorting when the categories are initially loaded and each time they are updated.

--- src/category/components/store.ts
+++ src/category/components/store.ts
@@ -16,12 +16,21 @@ export const defaultInitState: CategoryState = {
   categories: [],
 };
 
+function sortCategories(categories: Category[]) {
+  return categories.sort((a, b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+    return 0;
+  });
+}
+
 export const createCategoryStore = (
   initState: CategoryState = defaultInitState,
 ) => {
   return createStore<CategoryStore>()((set) => ({
     ...initState,
+    categories: sortCategories(initState.categories),
     setCategories: (categories: Category[]) =>
-      set({ categories: [...categories] }),
+      set({ categories: [...sortCategories(categories)] }),
   }));
 };
