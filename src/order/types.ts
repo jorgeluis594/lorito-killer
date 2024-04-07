bb918c34ae8e10e38c381112b2cfbf4ab8a37cc3
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-07 14:33:13 -0500

    Add payment-related types to order types
    Extended the order types to include information about payment, introducing types for payment method, general payment data and specific payment types. The latter includes credit card, debit card, wallet, and cash payments. This enhancement enables a more detailed representation of order payment information.

--- src/order/types.ts
+++ src/order/types.ts
@@ -14,6 +14,42 @@ export type Order = {
   orderItems: OrderItem[];
   total: number;
   status: "pending" | "completed" | "cancelled";
+  payments: Payment[];
   createdAt?: Date;
   updatedAt?: Date;
 };
+
+export type PaymentMethod = "cash" | "credit_card" | "debit_card" | "wallet";
+
+export type PaymentGeneralData = {
+  id?: string;
+  orderId: string;
+  amount: number;
+  method: PaymentMethod;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type CreditCardPayment = PaymentGeneralData & {
+  method: "credit_card";
+};
+
+export type DebitCardPayment = PaymentGeneralData & {
+  method: "debit_card";
+};
+
+export type WalletPayment = PaymentGeneralData & {
+  method: "wallet";
+};
+
+export type CashPayment = PaymentGeneralData & {
+  method: "cash";
+  received_amount: number;
+  change: number;
+};
+
+export type Payment =
+  | CreditCardPayment
+  | DebitCardPayment
+  | WalletPayment
+  | CashPayment;
