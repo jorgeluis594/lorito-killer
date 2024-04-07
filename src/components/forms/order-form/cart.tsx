613b387b3c3dfa2039532d8971a87491d57ad8ec
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-06 23:06:10 -0500

    Add "Empty Cart" button to order form
    A button has been added to the cart section of the order form. This button, labeled "Empty Cart", provides users with the ability to easily reset their current order.

--- src/components/forms/order-form/cart.tsx
+++ src/components/forms/order-form/cart.tsx
@@ -34,8 +34,11 @@ export default function Cart() {
 
   return (
     <div className="h-full border-l grid grid-rows-[min-content_1fr_min-content]">
-      <div className="p-5 border-b">
+      <div className="p-5 border-b flex justify-between">
         <h2 className="text-xl font-semibold tracking-tight">Pedido</h2>
+        <Button variant="ghost" type="button" size="sm" onClick={() => reset()}>
+          Vaciar carrito
+        </Button>
       </div>
       <ScrollArea className="border-b">
         <div className="py-3 h-full">
