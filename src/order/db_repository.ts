d1606d11a40ab113a7e36254f1385335c92b88d7
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-08 08:42:42 -0500

    Add payment mapping functions to order repository
    Implemented new functions to map Payment and related types to their Prisma counterparts, and vice versa. These are used in the 'create' function of the order repository. The purpose is to provide a cleaner way to convert these types and handle type mismatches, notably for the 'cash' payment method.

--- src/order/db_repository.ts
+++ src/order/db_repository.ts
@@ -2,6 +2,9 @@ import type { Order, OrderItem } from "@/order/types";
 import { response, successResponse } from "@/lib/types";
 import prisma from "@/lib/prisma";
 import { Product } from "@/product/types";
+import { $Enums, type Payment as PrismaPayment, Prisma } from "@prisma/client";
+import { Payment } from "@/order/types";
+import PaymentMethod = $Enums.PaymentMethod;
 
 async function addOrderItem(
   orderId: string,
@@ -23,14 +26,62 @@ async function addOrderItem(
   }
 }
 
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+type PaymentPrismaMatch = Optional<
+  PrismaPayment,
+  "id" | "orderId" | "createdAt" | "updatedAt" | "data"
+>;
+
+function mapPaymentToPrisma(payment: Payment): PaymentPrismaMatch {
+  if (payment.method === "cash") {
+    const { received_amount, change, ...paymentData } = payment;
+    return {
+      ...paymentData,
+      method: payment.method.toUpperCase() as PaymentMethod,
+      amount: new Prisma.Decimal(paymentData.amount),
+      data: { received_amount, change },
+    };
+  } else {
+    return {
+      ...payment,
+      amount: new Prisma.Decimal(payment.amount),
+      method: payment.method.toUpperCase() as PaymentMethod,
+    };
+  }
+}
+
+function mapPrismaToPayment(prismaPayment: PaymentPrismaMatch): Payment {
+  if (prismaPayment.method === "CASH") {
+    return {
+      ...prismaPayment,
+      amount: prismaPayment.amount.toNumber(),
+      method: prismaPayment.method.toLowerCase() as any,
+      received_amount: ((prismaPayment.data as any) || {}).received_amount,
+      change: ((prismaPayment.data as any) || {}).change,
+    };
+  } else {
+    return {
+      ...prismaPayment,
+      amount: prismaPayment.amount.toNumber(),
+      method: prismaPayment.method.toLowerCase() as any,
+    };
+  }
+}
+
+function mapPaymentsToPrisma(payments: Payment[]): PaymentPrismaMatch[] {
+  return payments.map(mapPaymentToPrisma);
+}
+
 export const create = async (order: Order): Promise<response<Order>> => {
   try {
-    const { orderItems, ...orderData } = order;
+    const { orderItems, payments, ...orderData } = order;
 
     const createdOrderResponse = await prisma.order.create({
       data: {
         ...orderData,
+        payments: { create: mapPaymentsToPrisma(payments) as any },
       },
+      include: { payments: true },
     });
     const createdOrderItemsResponses = await Promise.all(
       orderItems.map((oi) => addOrderItem(createdOrderResponse.id, oi)),
@@ -40,6 +91,7 @@ export const create = async (order: Order): Promise<response<Order>> => {
       ...createdOrderResponse,
       total: createdOrderResponse.total.toNumber(),
       status: order.status,
+      payments: createdOrderResponse.payments.map(mapPrismaToPayment),
       orderItems: [],
     };
 
