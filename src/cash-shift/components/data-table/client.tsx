fbb3d9727cb53df8303bc122ac81d9719a836232
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-15 01:24:06 -0500

    Add lazing cash shifts loading

--- src/cash-shift/components/data-table/client.tsx
+++ src/cash-shift/components/data-table/client.tsx
@@ -4,13 +4,31 @@ import { columns } from "@/cash-shift/components/data-table/columns";
 import { DataTable } from "@/components/ui/data-table";
 import { useState, useEffect } from "react";
 import { CashShiftWithOutOrders } from "@/cash-shift/types";
+import { getManyCashShifts } from "@/cash-shift/api_repository";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function TableClient() {
-  const [loading, isLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { toast } = useToast();
   const [cashShifts, setCashShifts] = useState<CashShiftWithOutOrders[]>([]);
 
   useEffect(() => {
-    async function fetchData() {}
+    async function fetchData() {
+      const response = await getManyCashShifts();
+      if (!response.success) {
+        toast({
+          title: "Error",
+          description:
+            "Ocurrió un error al cargar las cajas: " + response.message,
+          variant: "destructive",
+        });
+        return;
+      } else {
+        setCashShifts(response.data);
+      }
+
+      setLoading(false);
+    }
 
     fetchData();
   }, []);
