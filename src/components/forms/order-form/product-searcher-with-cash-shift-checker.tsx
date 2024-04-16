fce0cd4bc4688f82990a7ad63286a61bd54889a8
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-15 22:25:05 -0500

    Fix hydratation error
    The error was raised because the hr element it mustn't be within p element.

--- src/components/forms/order-form/product-searcher-with-cash-shift-checker.tsx
+++ src/components/forms/order-form/product-searcher-with-cash-shift-checker.tsx
@@ -6,12 +6,14 @@ import CashShiftForm from "@/cash-shift/components/form";
 
 const CashShiftIsNotOpen = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <p className="text-center text-lg text-gray-600">
-        No tienes una caja abierta, abre una para generar ventas
+    <div className="flex text-center items-center justify-center h-full">
+      <div>
+        <p className="text-center text-lg text-gray-600">
+          No tienes una caja abierta, abre una para generar ventas
+        </p>
         <hr className="my-4" />
         <CashShiftForm />
-      </p>
+      </div>
     </div>
   );
 };
