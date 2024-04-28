3957fd625885661c6a77a5c0afa24c2509db490e
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-28 15:53:52 -0500

    Add company db repository updateCompany method

--- src/company/db_repository.ts
+++ src/company/db_repository.ts
@@ -16,3 +16,34 @@ export const createCompany = async (
     return { success: false, message: e.message };
   }
 };
+
+export const updateCompany = async (
+  company: Company,
+): Promise<response<Company>> => {
+  try {
+    const updatedCompany = await prisma.company.update({
+      where: { id: company.id },
+      data: company,
+    });
+
+    return { success: true, data: updatedCompany };
+  } catch (e: any) {
+    return { success: false, message: e.message };
+  }
+};
+
+export const getCompany = async (id: string): Promise<response<Company>> => {
+  try {
+    const company = await prisma.company.findUnique({
+      where: { id },
+    });
+
+    if (!company) {
+      return { success: false, message: "Company not found" };
+    }
+
+    return { success: true, data: company };
+  } catch (e: any) {
+    return { success: false, message: e.message };
+  }
+};
