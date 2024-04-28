c976951cae50ee4a8c9207e0cb5597710e2a7e43
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-28 11:36:25 -0500

    Add settings page

---
+++ src/app/dashboard/(dashboard)/settings/page.tsx
@@ -0,0 +1,18 @@
+import { Separator } from "@/components/ui/separator";
+import UserForm from "@/user/components/user-form";
+import ChangePasswordForm from "@/user/components/change-password-form";
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <h3 className="text-lg font-medium">Perfil</h3>
+      <p className="text-sm text-muted-foreground">
+        Edita tus datos y cambia de contraseña
+      </p>
+      <Separator className="my-4" />
+      <UserForm />
+      <Separator className="mt-6 mb-4" />
+      <ChangePasswordForm />
+    </div>
+  );
+}
