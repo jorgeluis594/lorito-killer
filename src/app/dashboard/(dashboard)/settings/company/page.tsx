4880e5235a2f183fa5e9b62bf53710d9acf6687a
Jorge Gonzalez <jorg3.594@gmail.com>
2024-04-28 15:54:39 -0500

    Add company settings page

--- src/app/dashboard/(dashboard)/settings/company/page.tsx
+++ src/app/dashboard/(dashboard)/settings/company/page.tsx
@@ -1,7 +1,20 @@
 import { Separator } from "@/components/ui/separator";
 import CompanyForm from "@/company/components/company-form";
+import { getSession } from "@/lib/auth";
+import { getCompany } from "@/company/db_repository";
+import { notFound } from "next/navigation";
+
+export const revalidate = 0;
 
 export default async function CompanySettingsPage() {
+  const session = await getSession();
+  const companyResponse = await getCompany(session.user.companyId);
+
+  if (!companyResponse.success) {
+    notFound();
+    return;
+  }
+
   return (
     <div>
       <h3 className="text-lg font-medium">Empresa</h3>
@@ -9,7 +22,7 @@ export default async function CompanySettingsPage() {
         Edita los datos de tu empresa y crea nuevos usuarios
       </p>
       <Separator className="my-4" />
-      <CompanyForm />
+      <CompanyForm company={companyResponse.data} />
       <Separator className="mt-6 mb-4" />
     </div>
   );
