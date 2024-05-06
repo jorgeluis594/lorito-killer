653c207046caf7bd08c68fbcf5d1d7de461aff40
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-05 20:24:58 -0500

    Add PackageProductSchema and ProductItemSchema

--- src/product/schema.ts
+++ src/product/schema.ts
@@ -39,3 +39,36 @@ export const SingleProductSchema = z.object({
   updatedAt: z.date().optional(),
   createdAt: z.date().optional(),
 });
+
+export const ProductItemSchema = z.object({
+  id: z.string(),
+  productId: z.string(),
+  quantity: z.number(),
+});
+
+export const PackageProductSchema = z.object({
+  id: z.string().optional(),
+  companyId: z.string(),
+  name: z.string().min(3, {
+    message: "El nombre del producto debe tener al menos 3 caracteres",
+  }),
+  price: z.coerce.number().gt(0, "El producto debe tener un precio"),
+  description: z.string(),
+  sku: z
+    .string()
+    .min(3, { message: "El sku debe tener al menos 3 caracteres" })
+    .regex(/^[a-zA-Z0-9_]*$/, {
+      message: "SKU solo puede contener carácteres alfanuméricos y guión abajo",
+    })
+    .optional(),
+  photos: z
+    .array(PhotoSchema)
+    .max(IMG_MAX_LIMIT, { message: "You can only add up to 5 images" })
+    .optional(),
+  categories: z.array(CategorySchema),
+  productItems: z
+    .array(ProductItemSchema)
+    .min(1, { message: "Debe agregar al menos un producto" }),
+  updatedAt: z.date().optional(),
+  createdAt: z.date().optional(),
+});
