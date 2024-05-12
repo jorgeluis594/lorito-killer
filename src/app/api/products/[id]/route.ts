eb0d80380b57ab6b846f66f74753787d83287f4a
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-12 11:41:29 -0500

    ADd companyId param to find product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -7,12 +7,15 @@ import {
 import { Product } from "@/product/types";
 import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import { getSession } from "@/lib/auth";
 export async function PUT(
   req: Request,
   { params }: { params: { id: string } },
 ) {
+  const session = await getSession();
   const productData = (await req.json()) as Product;
-  const { success } = await findProduct(params.id);
+
+  const { success } = await findProduct(params.id, session.user.companyId);
   if (!success) {
     return NextResponse.json(
       { success: false, message: "Product not found" },
@@ -22,7 +25,10 @@ export async function PUT(
 
   const responsed = await findBy({ sku: productData.sku });
   if (responsed.success && responsed.data.id !== productData.id) {
-    return NextResponse.json({ success: false, message: "Ya existe un producto con el sku" });
+    return NextResponse.json({
+      success: false,
+      message: "Ya existe un producto con el sku",
+    });
   }
 
   const response = await UpdateProduct(productData);
@@ -34,7 +40,11 @@ export async function DELETE(
   _req: Request,
   { params }: { params: { id: string } },
 ) {
-  const findProductResponse = await findProduct(params.id);
+  const session = await getSession();
+  const findProductResponse = await findProduct(
+    params.id,
+    session.user.companyId,
+  );
   if (!findProductResponse.success) {
     return NextResponse.json(
       { success: false, message: "Product not found" },
@@ -52,9 +62,13 @@ export async function GET(
   _req: Request,
   { params }: { params: { id: string } },
 ) {
-  let response = await findProduct(params.id);
+  const session = await getSession();
+  let response = await findProduct(params.id, session.user.companyId);
   if (!response.success) {
-    response = await findBy({ sku: params.id });
+    response = await findBy({
+      sku: params.id,
+      companyId: session.user.companyId,
+    });
   }
 
   return NextResponse.json(response, { status: response.success ? 200 : 404 });
