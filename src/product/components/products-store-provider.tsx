be25c543bdd6e768b7f5805d0725ba67bda0d7d8
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-12 18:36:04 -0500

    Ad product loader

--- src/product/components/products-store-provider.tsx
+++ src/product/components/products-store-provider.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { type ReactNode, createContext, useRef, useContext } from "react";
+import {
+  type ReactNode,
+  createContext,
+  useRef,
+  useContext,
+  useEffect,
+} from "react";
 import { type StoreApi, useStore } from "zustand";
 
 import {
   type ProductsStore,
   createProductsStore,
 } from "@/product/components/store";
+import { useToast } from "@/shared/components/ui/use-toast";
+import { getLastOpenCashShift } from "@/cash-shift/api_repository";
+import { getMany as getManyProducts } from "@/product/api_repository";
 
 export const ProductsStoreContext =
   createContext<StoreApi<ProductsStore> | null>(null);
@@ -14,6 +23,28 @@ export const ProductsStoreContext =
 export interface ProductsStoreProviderProps {
   children: ReactNode;
 }
+
+const ProductsLoader = ({ children }: { children: ReactNode }) => {
+  const { toast } = useToast();
+  const { isLoading, setProducts } = useProductsStore((store) => store);
+
+  useEffect(() => {
+    if (isLoading) {
+      getManyProducts().then((response) => {
+        if (response.success) {
+          setProducts(response.data);
+        } else {
+          setProducts([]);
+          toast({
+            description: "No se pudo obtener los productos",
+          });
+        }
+      });
+    }
+  }, []);
+
+  return <>{children}</>;
+};
 
 export const ProductsStoreProvider = ({
   children,
@@ -25,7 +56,7 @@ export const ProductsStoreProvider = ({
 
   return (
     <ProductsStoreContext.Provider value={storeRef.current}>
-      {children}
+      <ProductsLoader>{children}</ProductsLoader>
     </ProductsStoreContext.Provider>
   );
 };
