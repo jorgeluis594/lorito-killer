10951d5f21be85af14a8f9437589c96372a7e258
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-12 20:23:58 -0500

    Add multiple items to product items selector

--- src/product/components/form/product-items-selector.tsx
+++ src/product/components/form/product-items-selector.tsx
@@ -13,6 +13,7 @@ import {
   SelectValue,
 } from "@/shared/components/ui/select";
 import { Input } from "@/shared/components/ui/input";
+import { useToast } from "@/shared/components/ui/use-toast";
 
 interface ProductItemsSelectorProps {
   value: ProductItem[];
@@ -24,12 +25,45 @@ export default function ProductItemsSelector({
   onChange,
 }: ProductItemsSelectorProps) {
   const singleProducts = useSingleProducts();
+  const { toast } = useToast();
 
   const addProduct = () => {
     onChange([
       ...value,
       { id: crypto.randomUUID(), quantity: 1, productId: "", productName: "" },
     ]);
+  };
+
+  const onProductChange = (productItemId: string, productId: string) => {
+    const updatedValue = value.map((productItem) => {
+      if (productItem.id !== productItemId) return productItem;
+
+      const product = singleProducts.find(
+        (product) => product.id === productId,
+      );
+
+      if (!product) {
+        toast({
+          title: "Error",
+          variant: "destructive",
+          description: "No se encontró el producto",
+        });
+        return productItem;
+      }
+
+      return { ...productItem, productId, productName: product.name };
+    });
+
+    onChange(updatedValue);
+  };
+
+  const onQuantityChange = (productItemId: string, quantity: number) => {
+    const updatedValue = value.map((productItem) => {
+      if (productItem.id !== productItemId) return productItem;
+      return { ...productItem, quantity };
+    });
+
+    onChange(updatedValue);
   };
 
   return (
@@ -41,30 +75,43 @@ export default function ProductItemsSelector({
         </Button>
       </div>
       <div className="min-h-20 ">
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-8">
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Escoja un producto" />
-              </SelectTrigger>
-              <SelectContent>
-                {singleProducts.map((product) => (
-                  <SelectItem key={product.id} value={product.id!}>
-                    {product.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
+        {value.map((productItem) => (
+          <div key={productItem.id} className="grid grid-cols-12 gap-4">
+            <div className="col-span-8">
+              <Select
+                onValueChange={(productId) =>
+                  onProductChange(productItem.id, productId)
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Escoja un producto" />
+                </SelectTrigger>
+                <SelectContent>
+                  {singleProducts.map((product) => (
+                    <SelectItem key={product.id} value={product.id!}>
+                      {product.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="col-span-3">
+              <Input
+                placeholder="Cantidad"
+                type="number"
+                required
+                onChange={(e) =>
+                  onQuantityChange(productItem.id, parseInt(e.target.value))
+                }
+              />
+            </div>
+            <div className="col-span-1">
+              <Button variant="destructive" size="icon">
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
           </div>
-          <div className="col-span-3">
-            <Input placeholder="Cantidad" type="number" required />
-          </div>
-          <div className="col-span-1">
-            <Button variant="destructive" size="icon">
-              <Trash className="h-4 w-4" />
-            </Button>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
