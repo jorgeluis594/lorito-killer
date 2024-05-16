e2a0a313b065237482ea2d5fcc7df3e0aa94bd9a
PatrickFt <freytas04@gmail.com>
2024-05-16 18:32:16 -0500

    Add category modal

---
+++ src/category/components/category-modal/category-modal.tsx
@@ -0,0 +1,29 @@
+
+import { useCategoryStore } from "@/category/components/category-store-provider";
+import ListCategories from "./category";
+import { Dialog, DialogContent, DialogTrigger } from "@/shared/components/ui/dialog";
+import { Button } from "@/shared/components/ui/button";
+import { ScrollArea } from "@/shared/components/ui/scroll-area";
+import { Plus } from "lucide-react";
+
+export default function CategoryModal({
+  onActionPerformed,
+}: {
+  onActionPerformed: () => void;
+}) {
+  const productType = useCategoryStore((store) => store.categories);
+  
+  return (
+    <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="text-xs md:text-sm"><Plus className="mr-2 h-4 w-4" />Mostrar Categorías</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[750px] sm:h-[750px] w-full flex flex-col justify-center items-center p-0">
+          <ScrollArea className="p-6">
+            <ListCategories/>
+          </ScrollArea>
+        </DialogContent>
+      </Dialog>
+  )
+}
+  
