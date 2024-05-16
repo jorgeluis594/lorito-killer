d00f653abf4561c4170880c5749172150ad07706
PatrickFt <freytas04@gmail.com>
2024-05-16 18:25:37 -0500

    Render in category table

---
+++ src/category/components/category-modal/category.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { Suspense, useEffect, useState } from "react"
+import { Category } from "@/category/types"
+import { getMany } from "@/category/api_repository"
+import CategoriesClient from "@/category/components/data-table/client"
+
+const Categories = () => {
+  const [categories, setCategories] = useState<null | Category[]>(null)
+  const [error, setError] = useState<null | string>();
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchCategories = async () => {
+    const response = await getMany();
+    setIsLoading(false);
+
+    if (!response.success) {
+      setError(response.message);
+    } else {
+      setCategories(response.data);
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const reloadCategories = () => {
+    fetchCategories();
+  };
+
+  return (
+    <>
+      <CategoriesClient
+        data={categories}
+        isLoading={isLoading}
+        onUpsertProductPerformed={reloadCategories} />
+    </>
+  )
+}
+
+export default function ListCategories() {
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <Categories />
+    </Suspense>
+  )
+}
