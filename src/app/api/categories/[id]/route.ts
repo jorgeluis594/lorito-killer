4abfa176d9c8683ed801f46bc1758c6261bd5c89
PatrickFt <freytas04@gmail.com>
2024-05-20 17:55:21 -0500

    Add put in route

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -1,10 +1,33 @@
 import {
     deleteCategory,
     find as findCategory,
+    update as UpdateCategory,
   } from "@/category/db_respository";
 import { NextResponse } from "next/server";
 import { getSession } from "@/lib/auth";
 import { revalidatePath } from "next/cache";
+import { Category } from "@/category/types";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const categoryData: Category = await req.json();
+
+  const findProductResponse = await findCategory(params.id);
+  if (!findProductResponse.success) {
+    return NextResponse.json(
+      { success: false, message: "Category not found" },
+      { status: 404 },
+    );
+  }
+
+  const updateResponse = await UpdateCategory(categoryData);
+  revalidatePath("/categories/" + params.id);
+  return NextResponse.json(updateResponse, {
+    status: updateResponse.success ? 200 : 400,
+  });
+}
 
 export async function DELETE(
     _req: Request,
