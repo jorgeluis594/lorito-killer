27d62274a16675dbf0210dab6fc7ee3edaddd42b
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-20 22:52:43 -0500

    Add addOrderItem and updateOrderItem to order form store

--- src/new-order/store.ts
+++ src/new-order/store.ts
@@ -1,6 +1,6 @@
 import { createStore } from "zustand/vanilla";
 
-import { Order, Payment, PaymentMethod } from "@/order/types";
+import { Order, OrderItem, Payment, PaymentMethod } from "@/order/types";
 import { Product } from "@/product/types";
 import { response } from "@/lib/types";
 import { CashShift } from "@/cash-shift/types";
@@ -13,6 +13,8 @@ export type OrderFormStore = {
 export type Actions = {
   addProduct: (product: Product) => void;
   removeOrderItem: (orderItemId: string) => void;
+  addOrderItem: (orderItem: OrderItem) => void;
+  updateOrderItem: (orderItem: OrderItem) => void;
   reset: () => void;
   increaseQuantity: (productId: string) => void;
   decreaseQuantity: (productId: string) => void;
