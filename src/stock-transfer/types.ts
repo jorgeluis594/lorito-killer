da3ebba9d10ff894d4a58d98dcbdd97cf775cb09
Jorge Gonzalez <jorg3.594@gmail.com>
2024-05-26 19:00:41 -0500

    Add orderItemId to OrderStockTransfer

--- src/stock-transfer/types.ts
+++ src/stock-transfer/types.ts
@@ -6,8 +6,15 @@ export type StockTransferBase = {
 export const OrderStockTransfer = "OrderStockTransfer";
 export type OrderStockTransferType = typeof OrderStockTransfer;
 export type OrderStockTransfer = StockTransferBase & {
-  orderId: string;
   orderItemId: string;
-  productId: string;
+  fromProductId: string;
   type: OrderStockTransferType;
+};
+
+export const ProductStockTransfer = "ProductStockTransfer";
+export type ProductStockTransferType = typeof ProductStockTransfer;
+export type ProductStockTransfer = StockTransferBase & {
+  fromProductId: string;
+  toProductId: string;
+  type: ProductStockTransferType;
 };
