a33af726162819c390c8baf240da547271e7fddf
PatrickFt <freytas04@gmail.com>
2024-05-29 13:04:04 -0500

    Add restriction when logout with cash shift open

--- src/shared/components/layout/sign-out.tsx
+++ src/shared/components/layout/sign-out.tsx
@@ -1,7 +1,26 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { toast, useToast } from "../ui/use-toast";
+import { getLastOpenCashShift } from "@/cash-shift/api_repository";
+import { useCallback } from "react";
 
 export default function SignOut() {
-  return <div onClick={() => signOut()}>Cerrar sesión</div>;
+
+  const { toast } = useToast();
+  const handleClick = useCallback(async () => {
+    const response = await getLastOpenCashShift();
+    if (response.success) {
+      toast({
+        title: "Tiene una caja abierta",
+        description: "Necesita cerrar la caja antes de cerrar sesion",
+        variant: "destructive",
+      });
+    } else {
+      signOut()
+    }
+  }, [toast]);
+
+
+  return <div onClick={handleClick}>Cerrar sesión</div>;
 }
