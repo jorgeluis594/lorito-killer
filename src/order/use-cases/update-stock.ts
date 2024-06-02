476a63da710cd9c4f8cf2ab455d797085ba68fc5
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-01 22:58:18 -0500

    Add updateStock use case

---
+++ src/order/use-cases/update-stock.ts
@@ -0,0 +1,47 @@
+import { Order } from "@/order/types";
+import { response } from "@/lib/types";
+import { type StockTransfer } from "@/stock-transfer/types";
+import { Product } from "@/product/types";
+import {
+  generateOrderStocksTransfers,
+  validateStockTransfers,
+} from "@/stock-transfer/use-cases/generate-order-stock-transfer";
+
+interface Repository {
+  findProduct: (productId: string) => Promise<response<Product>>;
+  updateStock: (stockTransfer: StockTransfer) => Promise<response<undefined>>;
+}
+
+export const updateStock = async (
+  order: Order,
+  repository: Repository,
+): Promise<response<undefined>> => {
+  const stockTransfersResponse = await generateOrderStocksTransfers(
+    order,
+    repository.findProduct,
+  );
+  if (!stockTransfersResponse.success) {
+    return stockTransfersResponse;
+  }
+
+  const stockTransfers = stockTransfersResponse.data;
+
+  const validateResponse = await validateStockTransfers(
+    stockTransfers,
+    repository.findProduct,
+  );
+  if (!validateResponse.success) {
+    return validateResponse;
+  }
+  if (!validateResponse.data) {
+    return { success: false, message: "Invalid stock transfers" };
+  }
+
+  await Promise.all(
+    stockTransfers.map((stockTransfer) =>
+      repository.updateStock(stockTransfer),
+    ),
+  );
+
+  return { success: true, data: undefined };
+};
