7b177f59bf2c43be1fc331abf2a97d6237979915
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-11 22:18:10 -0500

    Add useSignOut

--- src/shared/components/layout/sign-out.tsx
+++ src/shared/components/layout/sign-out.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import useSignOut from "@/lib/use-sign-out";
 
 export default function SignOut() {
+  const signOut = useSignOut();
   return <div onClick={() => signOut()}>Cerrar sesión</div>;
 }
