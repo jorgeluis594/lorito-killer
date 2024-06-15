bff09f74ef3ff14ad678ffc955881c4d5ee2001e
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-15 10:04:59 -0500

    Handle logic to redirect to subdomain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-import { getServerSession } from "next-auth";
+import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { getCompany } from "@/company/db_repository";
 
-export default async function Page() {
-  const session = await getServerSession();
+export default async function Page({ ...params }: any) {
+  const session = await getSession();
 
   if (session) {
-    return redirect("/dashboard");
+    const companyResponse = await getCompany(session.user.companyId);
+    if (!companyResponse.success) {
+      return;
+    }
+
+    return redirect(
+      `${process.env.NODE_ENV === "development" ? "http" : "https"}://${companyResponse.data.subdomain}.${process.env.DOMAIN}/dashboard`,
+    );
   } else {
-    return redirect("/login");
+    return (
+      <h1>
+        Ingresa tu subdominio en la barra de dirección. Ejemplo:
+        https://market-chavez.kogoz.pe
+      </h1>
+    );
   }
 }
