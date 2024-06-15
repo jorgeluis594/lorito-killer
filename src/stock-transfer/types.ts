9b1dab22172a700b02cc9b55b661813924171ae7
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-15 15:03:54 -0500

    Add createdAt to StockTransferBase

--- src/stock-transfer/types.ts
+++ src/stock-transfer/types.ts
@@ -13,6 +13,7 @@ export type StockTransferBase = {
   value: number;
   productId: string;
   type: OrderStockTransferType | ProductStockTransferType;
+  createdAt: Date;
 };
 
 export type OrderStockTransfer = StockTransferBase & {
