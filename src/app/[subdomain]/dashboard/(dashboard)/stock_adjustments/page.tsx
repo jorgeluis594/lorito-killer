96066fa15f204bf0a8db68651fdbed038ba7d172
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-15 16:29:07 -0500

    Add logic to stock_adjusments datatable

--- src/app/[subdomain]/dashboard/(dashboard)/stock_adjustments/page.tsx
+++ src/app/[subdomain]/dashboard/(dashboard)/stock_adjustments/page.tsx
@@ -3,12 +3,32 @@ import { Heading } from "@/shared/components/ui/heading";
 import { Separator } from "@/shared/components/ui/separator";
 import { DataTable } from "@/stock-transfer/components/table/client";
 import { columns } from "@/stock-transfer/components/table/columns";
+import { getMany, total } from "@/stock-transfer/db_repository";
+import { getSession } from "@/lib/auth";
 
 const breadcrumbItems = [
   { title: "Ajustes de stock", link: "/stock_adjustments" },
 ];
 
-export default async function Page() {
+type paramsProps = {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+};
+
+export default async function Page({ searchParams }: paramsProps) {
+  const page = Number(searchParams.page) || 1;
+  const pageLimit = Number(searchParams.limit) || 10;
+  const session = await getSession();
+  const [resultStockTransfers, totalCount] = await Promise.all([
+    getMany(session.user.companyId, page, pageLimit),
+    total(session.user.companyId),
+  ]);
+
+  if (!resultStockTransfers.success) {
+    return;
+  }
+
   return (
     <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
       <BreadCrumb items={breadcrumbItems} />
@@ -21,7 +41,11 @@ export default async function Page() {
       </div>
       <Separator />
       <div className="w-3/4 mx-auto">
-        <DataTable data={[]} columns={columns} pageCount={1} />
+        <DataTable
+          data={resultStockTransfers.data}
+          columns={columns}
+          pageCount={totalCount / pageLimit}
+        />
       </div>
     </div>
   );
