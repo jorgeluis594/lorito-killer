c8c88d20d4d979e3a57e805e2515cdcdcdc44204
Jorge Gonzalez <jorg3.594@gmail.com>
2024-06-30 23:25:28 -0500

    Add persisting and performing action for ProductMovementStockTransfer

--- src/stock-transfer/components/actions.ts
+++ src/stock-transfer/components/actions.ts
@@ -3,11 +3,16 @@
 import { find as findProduct } from "@/product/db_repository";
 import { processStockTransfer } from "@/stock-transfer/use-cases/process-stock-transfer";
 import {
-  updateStock,
   create as createStockTransfer,
+  updateStock,
 } from "@/stock-transfer/db_repository";
-import { StockTransfer } from "@/stock-transfer/types";
+import {
+  ProductMovementStockTransfer,
+  ProductMovementStockTransferName,
+  StockTransfer,
+} from "@/stock-transfer/types";
 import { response } from "@/lib/types";
+import { SingleProduct } from "@/product/types";
 
 export const createAndProcessStockTransfers = async (
   stockTransfers: StockTransfer[],
@@ -24,4 +29,57 @@ export const createAndProcessStockTransfers = async (
       }),
     ),
   );
+};
+
+export const performProductMovementStockTransfer = async (
+  parentProduct: SingleProduct,
+): Promise<response<StockTransfer[]>> => {
+  if (!parentProduct.stockConfig) {
+    return { success: false, message: "Product has no stock configuration" };
+  }
+
+  const receiverProductResponse = await findProduct(
+    parentProduct.stockConfig.productId,
+  );
+
+  if (!receiverProductResponse.success) {
+    return receiverProductResponse;
+  }
+
+  const stockTransfers: ProductMovementStockTransfer[] = [
+    // Remove stock from parent product
+    {
+      id: crypto.randomUUID(),
+      productId: parentProduct.id!,
+      companyId: parentProduct.companyId!,
+      value: -1,
+      type: ProductMovementStockTransferName,
+      productName: parentProduct.name,
+      fromProductId: parentProduct.id!,
+      toProductId: receiverProductResponse.data.id!,
+      createdAt: new Date(),
+    },
+    // Add stock to receiver product
+    {
+      id: crypto.randomUUID(),
+      productId: receiverProductResponse.data.id!,
+      companyId: receiverProductResponse.data.companyId!,
+      value: parentProduct.stockConfig.quantity,
+      type: ProductMovementStockTransferName,
+      productName: receiverProductResponse.data.name,
+      fromProductId: parentProduct.id!,
+      toProductId: receiverProductResponse.data.id!,
+      createdAt: new Date(),
+    },
+  ];
+
+  const performStockTransferResponses =
+    await createAndProcessStockTransfers(stockTransfers);
+
+  console.log(performStockTransferResponses);
+  if (performStockTransferResponses.some((response) => !response.success)) {
+    return { success: false, message: "Error" };
+  }
+
+  return { success: true, data: stockTransfers };
 };
