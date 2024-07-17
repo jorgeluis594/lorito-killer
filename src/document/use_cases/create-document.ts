e890555a2b19231beaf813c2f2341901c685d856
PatrickFt <freytas04@gmail.com>
2024-07-16 21:09:57 -0500

    Improve the logic to validate if it is a receipt or invoice

--- src/document/use_cases/create-document.ts
+++ src/document/use_cases/create-document.ts
@@ -1,22 +1,31 @@
 "use server";
 
 import {response} from "@/lib/types";
-import {Order} from "@/order/types";
+import {Order, OrderWithCustomer} from "@/order/types";
+import {Company} from "@/company/types";
+import {hasCustomer} from "@/order/utils";
 
 interface DocumentRepository {
-    createInvoice: (order: Order) => Promise<response<Order>>;
-    createReceipt: (order: Order) => Promise<response<Order>>;
+    createInvoice: (order: OrderWithCustomer, company: Company) => Promise<response<Order>>;
+    createReceipt: (order: Order, company: Company) => Promise<response<Order>>;
 }
 
 export const createDocument = async (
     repository: DocumentRepository,
     order: Order,
+    company: Company,
 ): Promise<response<Order>> => {
-    if (order.documentType === "invoice") {
-        return repository.createInvoice(order);
-    } else if(order.documentType === "receipt") {
-        return repository.createReceipt(order);
-    } else {
+    if (order.documentType !== 'receipt' || 'invoice') {
         return { success: false, message: "not implemented"};
     }
+
+    if (order.documentType == 'receipt') {
+        return repository.createReceipt(order, company);
+    }
+
+    if (!hasCustomer(order)) {
+        return { success: false, message: "No customer found" }
+    }
+
+    return repository.createInvoice(order, company);
 };
