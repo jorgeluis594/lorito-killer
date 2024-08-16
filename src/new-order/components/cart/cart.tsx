f1b4300caf7cbbd14a48244652f9cc86ce063290
PatrickFt <freytas04@gmail.com>
2024-08-15 19:09:48 -0500

    Use setDocumentType in the tab

--- src/new-order/components/cart/cart.tsx
+++ src/new-order/components/cart/cart.tsx
@@ -9,25 +9,30 @@ import {
 import TicketView from "@/new-order/components/document-view/ticket-view";
 import InvoiceView from "@/new-order/components/document-view/invoice-view";
 import ReceiptView from "@/new-order/components/document-view/receipt-view";
+import {useOrderFormActions, useOrderFormStore} from "@/new-order/order-form-provider";
 
 export default function Cart() {
+
+  const order = useOrderFormStore((state) => state.order);
+
+  const { setDocumentType } = useOrderFormActions();
 
   return (
     <>
       <div className="border-l h-full">
-        <Tabs defaultValue="ticket">
+        <Tabs defaultValue="ticket" onValueChange={() => setDocumentType(order.documentType)}>
           <TabsList className="grid w-full grid-cols-3">
             <TabsTrigger value="ticket">Nota de Venta</TabsTrigger>
             <TabsTrigger value="receipt">Boleta</TabsTrigger>
             <TabsTrigger value="invoice">Factura</TabsTrigger>
           </TabsList>
-          <TabsContent value="ticket">
+          <TabsContent value="ticket" className="h-[40rem]">
             <TicketView/>
           </TabsContent>
-          <TabsContent value="receipt">
+          <TabsContent value="receipt" className="h-[40rem]">
             <ReceiptView/>
           </TabsContent>
-          <TabsContent value="invoice">
+          <TabsContent value="invoice" className="h-[40rem]">
             <InvoiceView/>
           </TabsContent>
         </Tabs>
