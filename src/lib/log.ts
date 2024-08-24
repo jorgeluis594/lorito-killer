b26cf7babd1b827e0db36ee4a33e2915b1d1ec55
Jorge Gonzalez <jorg3.594@gmail.com>
2024-08-24 11:45:23 -0500

    Improve logger utility

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,6 +1,6 @@
-import pino, { Logger } from "pino";
+import pino, { BaseLogger, Logger } from "pino";
 
-export const logger: Logger =
+export const pinoLogger: Logger =
   process.env["NODE_ENV"] === "production"
     ? pino()
     : pino({
@@ -12,3 +12,17 @@ export const logger: Logger =
         },
         level: "debug",
       });
+
+const logger = (
+  type: "info" | "warn" | "debug" | "fatal" | "error",
+  event: string,
+  data: any,
+) => pinoLogger[type]({ ...data, event });
+
+export const log = {
+  info: (event: string, data: any) => logger("info", event, data),
+  warn: (event: string, data: any) => logger("warn", event, data),
+  debug: (event: string, data: any) => logger("debug", event, data),
+  fatal: (event: string, data: any) => logger("fatal", event, data),
+  error: (event: string, data: any) => logger("error", event, data),
+};
