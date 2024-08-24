770897242f87c97e7f9b984d324e612fa92789d2
Jorge Gonzalez <jorg3.594@gmail.com>
2024-08-24 17:58:17 -0500

    Refactor set type for createReceipt and createInvoice

--- src/document/factpro/gateway.ts
+++ src/document/factpro/gateway.ts
@@ -3,12 +3,14 @@ import { response } from "@/lib/types";
 import { format, parse } from "date-fns";
 import axios from "axios";
 import { Company } from "@/company/types";
-import { Document } from "@/document/types";
+import type { Invoice, Receipt } from "@/document/types";
 import type {
   FactproDocument,
   FactproDocumentItem,
 } from "@/document/factpro/types";
 import { log } from "@/lib/log";
+import { Customer } from "@/customer/types";
+import { isBusinessCustomer } from "@/customer/utils";
 
 const url = process.env.FACTPRO_URL;
 const token = process.env.FACTPRO_TOKEN;
@@ -17,7 +19,7 @@ const token = process.env.FACTPRO_TOKEN;
 export const createInvoice = async (
   order: OrderWithBusinessCustomer,
   company: Company,
-): Promise<response<Document>> => {
+): Promise<response<Invoice>> => {
   const body: FactproDocument = {
     tipo_documento: "01",
     serie: "NV01", // Falta configurar, primero se debe hacer la prueba de concepto
@@ -86,9 +88,99 @@ export const createInvoice = async (
 export const createReceipt = async (
   order: Order,
   company: Company,
-): Promise<response<Document>> => {
-  return { success: false, message: "createReceipt" };
+): Promise<response<Receipt>> => {
+  const body: FactproDocument = {
+    tipo_documento: "01",
+    serie: "NV01", // Falta configurar, primero se debe hacer la prueba de concepto
+    numero: "1", // Falta crear algoritmo de asignación de numero
+    tipo_operacion: "0101", // By default
+    fecha_de_emision: format(order.createdAt!, "dd/MM/yyyy"),
+    hora_de_emision: format(order.createdAt!, "hh:mm:ss"),
+    moneda: "PEN",
+    enviar_automaticamente_al_cliente: false,
+    datos_del_emisor: {
+      codigo_establecimiento: "0000", // Falta configurar el codigo de establecimiento
+    },
+    cliente: clientParamsBuilder(order.customer),
+    totales: {
+      total_exoneradas: order.total,
+      total_tax: 0,
+      total_venta: order.total,
+    },
+    items: order.orderItems.map((orderItem) =>
+      orderItemToDocumentItem(orderItem),
+    ),
+    acciones: {
+      formato_pdf: "a4",
+    },
+    termino_de_pago: {
+      descripcion: "Contado",
+      tipo: "0",
+    },
+    metodo_de_pago: "Efectivo", // agregar metodo de pago
+  };
+
+  const response = await sendDocument(body, order.id!);
+  if (!response.success) {
+    return response;
+  }
+
+  return {
+    success: true,
+    data: {
+      id: crypto.randomUUID(),
+      orderId: order.id!,
+      customerId: order.customer?.id!,
+      netTotal: body.totales.total_exoneradas,
+      taxTotal: body.totales.total_tax,
+      total: body.totales.total_venta,
+      documentType: "receipt",
+      series: body.serie,
+      number: body.numero,
+      dateOfIssue: parse(
+        `${body.fecha_de_emision} ${body.hora_de_emision}`,
+        "yyyy-MM-dd HH:mm",
+        new Date(),
+      ),
+    },
+  };
 };
+
+function clientParamsBuilder(
+  customer: Customer | undefined,
+): FactproDocument["cliente"] {
+  if (!customer) {
+    return {
+      cliente_tipo_documento: "0",
+      cliente_numero_documento: "00000000",
+      cliente_denominacion: "-",
+      codigo_pais: "PE",
+      cliente_direccion: "-",
+    };
+  }
+
+  if (isBusinessCustomer(customer)) {
+    return {
+      cliente_tipo_documento: "6",
+      cliente_numero_documento: customer.documentNumber,
+      cliente_denominacion: customer.legalName,
+      codigo_pais: "PE",
+      cliente_direccion: customer.address,
+      cliente_email: customer.email,
+      cliente_telefono: customer.phoneNumber,
+    };
+  }
+
+  return {
+    cliente_tipo_documento: "1",
+    cliente_numero_documento: customer.documentNumber || "00000000",
+    cliente_denominacion: customer.fullName,
+    codigo_pais: "PE",
+    cliente_direccion: customer.address || "-",
+    cliente_email: customer.email,
+    cliente_telefono: customer.phoneNumber,
+  };
+}
 
 const orderItemToDocumentItem = (orderItem: OrderItem): FactproDocumentItem => {
   return {
