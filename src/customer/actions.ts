0860e566102e83c14d42b87f3e73e26802d6cd8b
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-08-25 22:48:17 -0500

    Refactor action from search

--- src/customer/actions.ts
+++ src/customer/actions.ts
@@ -1,23 +1,37 @@
 "use server";
 
-import { type Customer } from "@/customer/types";
-import { response } from "@/lib/types";
-import { createCustomer as persistCustomer } from "@/customer/db_repository";
+import {type Customer} from "@/customer/types";
+import {response} from "@/lib/types";
+import {createCustomer as persistCustomer} from "@/customer/db_repository";
 import {fetchCustomerByDNI, fetchCustomerByRuc} from "@/document/factpro_gateway";
+import {FetchCustomer} from "@/document/types";
 
 export const createCustomer = async (
   customer: Customer,
 ): Promise<response<Customer>> => persistCustomer(customer);
 
-export const searchCustomer = async  (
+export const searchCustomer = async (
   documentNumber: string,
   documentType: string,
-) => {
-  if(documentType === "invoice") {
-    fetchCustomerByRuc(documentNumber);
-    console.log("holi ruc")
-  }else{
-    fetchCustomerByDNI(documentNumber);
-    console.log("holi dni")
+): Promise<response<FetchCustomer>> => {
+  try {
+    const fetchFunction = documentType === "invoice" ? fetchCustomerByRuc : fetchCustomerByDNI;
+
+    const result = await fetchFunction(documentNumber);
+
+    if (result.success) {
+      return {
+        success: true,
+        data: {
+          ...result.data,
+          name: result.data.name,
+          address: result.data.address,
+        }
+      };
+    } else {
+      return { success: false, message: result.message || "No se encontró el cliente" };
+    }
+  } catch (e) {
+    return {success: false, message: "No se encontro el cliente"};
   }
 }
