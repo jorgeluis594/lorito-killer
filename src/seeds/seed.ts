586334a70dd691365ac9f3e5a614f09d7762a543
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-08-30 00:45:59 -0500

    Add execute to locality db

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,5 +1,46 @@
 import prisma from "../lib/prisma";
 import bcrypt from "bcrypt";
+import fs from 'fs';
+import csv from 'csv-parser';
+import {$Enums} from "@prisma/client";
+import PrismaLocalityType = $Enums.LocalityLevel;
+import {DEPARTMENT, DISTRICT, Local, LocalityLevelType, PROVINCE} from "./type";
+
+const CustomerDocumentTypeToPrismaMapper: Record<
+  LocalityLevelType,
+  PrismaLocalityType
+> = {
+  [DEPARTMENT]: PrismaLocalityType.DEPARTMENT,
+  [PROVINCE]: PrismaLocalityType.PROVINCE,
+  [DISTRICT]: PrismaLocalityType.DISTRICT,
+};
+
+const locality = async () => {
+  const results: Array<Local> = [];
+  fs.createReadStream('localities.csv')
+    .pipe(csv())
+    .on('data', (data: Local) => results.push(data))
+    .on('end', async () => {
+      console.log('CSV file successfully processed');
+
+      for (const result of results) {
+        await prisma.locality.create({
+          data: {
+            id: result.id,
+            idUbigeo: result.idUbigeo,
+            name: result.name,
+            code: result.code,
+            tag: result.tag,
+            searchValue: result.searchValue,
+            level: CustomerDocumentTypeToPrismaMapper[result.level],
+            parentId: result.parentId,
+          },
+        });
+      }
+
+      console.log('Data successfully inserted');
+    });
+}
 
 const execute = async () => {
   const company = await prisma.company.create({
@@ -23,6 +64,8 @@ const execute = async () => {
       name: "Jorge Gonzalez",
     },
   });
+
+  await locality();
 
   console.log(user);
 };
