e0d5305781894369cb0b27f9091467de7f24f98a
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-08-30 00:45:37 -0500

    Create type for Locality in seeds carpet

---
+++ src/seeds/type.ts
@@ -0,0 +1,24 @@
+export const DEPARTMENT = "1";
+
+export type DepartmentType = typeof DEPARTMENT;
+
+export const PROVINCE = "2";
+
+export type ProvinceType = typeof PROVINCE;
+
+export const DISTRICT = "3";
+
+export type DistrictType = typeof DISTRICT;
+
+export type LocalityLevelType = DepartmentType | ProvinceType | DistrictType;
+
+export type Local = {
+  id: string,
+  idUbigeo: string,
+  name: string,
+  code: string,
+  tag: string,
+  searchValue: string,
+  level: LocalityLevelType,
+  parentId: string,
+}
