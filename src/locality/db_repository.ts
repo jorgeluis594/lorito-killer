8d9e47cb0eb0cb6962c7adf49fef92a145a9f18c
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-08-31 07:43:00 -0500

    Add api_repository for search from LocalityLevel

--- src/locality/db_repository.ts
+++ src/locality/db_repository.ts
@@ -1,8 +1,19 @@
 import {response} from "@/lib/types";
 import prisma from "@/lib/prisma";
+import {$Enums, Locality as PrismaLocality, Prisma} from "@prisma/client";
+import {
+  COUNTRY,
+  DEPARTMENT,
+  DISTRICT,
+  Locality,
+  LocalityLevelType,
+  PROVINCE,
+  TypecountryType,
+  TypeDepartmentType,
+  TypeDistrictType,
+  TypeProvinceType
+} from "@/locality/types";
 import PrismaLocalityType = $Enums.LocalityLevel;
-import {$Enums} from "@prisma/client";
-import {COUNTRY, DEPARTMENT, DISTRICT, Locality, LocalityLevelType, PROVINCE} from "@/locality/types";
 
 const CustomerDocumentTypeToPrismaMapper: Record<
   PrismaLocalityType,
@@ -14,7 +25,98 @@ const CustomerDocumentTypeToPrismaMapper: Record<
   [PrismaLocalityType.DISTRICT]: DISTRICT,
 };
 
-export const find = async (id: string): Promise<response<Locality>> => {
+const prismaToLocality = async (
+  prismaLocality: PrismaLocality,
+): Promise<Locality> => {
+  if (prismaLocality.level === "COUNTRY") {
+    return {
+      _branch: "COUNTRY",
+      id: prismaLocality.id,
+      idUbigeo: prismaLocality.idUbigeo,
+      name: prismaLocality.name,
+      code: prismaLocality.code,
+      tag: prismaLocality.tag,
+      searchValue: prismaLocality.searchValue,
+      level: COUNTRY,
+      parentId: prismaLocality.parentId,
+    };
+  } else if (prismaLocality.level === "DEPARTMENT"){
+    return {
+      _branch: "DEPARTMENT",
+      id: prismaLocality.id,
+      idUbigeo: prismaLocality.idUbigeo,
+      name: prismaLocality.name,
+      code: prismaLocality.code,
+      tag: prismaLocality.tag,
+      searchValue: prismaLocality.searchValue,
+      level: DEPARTMENT,
+      parentId: prismaLocality.parentId,
+    };
+  } else if (prismaLocality.level === "PROVINCE"){
+    return {
+      _branch: "PROVINCE",
+      id: prismaLocality.id,
+      idUbigeo: prismaLocality.idUbigeo,
+      name: prismaLocality.name,
+      code: prismaLocality.code,
+      tag: prismaLocality.tag,
+      searchValue: prismaLocality.searchValue,
+      level: PROVINCE,
+      parentId: prismaLocality.parentId,
+    };
+  } else{
+    return {
+      _branch: "DISTRICT",
+      id: prismaLocality.id,
+      idUbigeo: prismaLocality.idUbigeo,
+      name: prismaLocality.name,
+      code: prismaLocality.code,
+      tag: prismaLocality.tag,
+      searchValue: prismaLocality.searchValue,
+      level: DISTRICT,
+      parentId: prismaLocality.parentId,
+    };
+  }
+};
+
+
+export const getMany = async ({
+  q,
+  localityLevel,
+}: {
+  q?: string | null;
+  localityLevel?: TypecountryType | TypeDepartmentType | TypeProvinceType | TypeDistrictType;
+}): Promise<response<Locality[]>> => {
+  try {
+    const query: Prisma.LocalityFindManyArgs = {
+      take: 20,
+      orderBy: [{ name: "asc" }],
+    };
+
+    if (localityLevel) {
+      query.where = {
+        ...query.where,
+      };
+    }
+
+    if (q)
+      query.where = {
+        ...query.where,
+        name: { contains: q, mode: "insensitive" },
+      };
+
+    const result = await prisma.locality.findMany({
+      ...query,
+    });
+    const locality = await Promise.all(result.map(prismaToLocality));
+
+    return { success: true, data: locality };
+  } catch (error: any) {
+    return { success: false, message: error.message };
+  }
+};
+
+/*export const find = async (id: string): Promise<response<Locality>> => {
   try {
     const findLocality = await prisma.locality.findUnique({ where: { id: id } });
     if (!findLocality) {
@@ -36,4 +138,4 @@ export const find = async (id: string): Promise<response<Locality>> => {
   } catch (e: any) {
     return { success: false, message: e.message };
   }
-}
+}*/
--- src/locality/types.ts
+++ src/locality/types.ts
@@ -16,7 +16,8 @@ export type DistrictType = typeof DISTRICT;
 
 export type LocalityLevelType = CountryType | DepartmentType | ProvinceType | DistrictType;
 
-export type Locality = {
+export type CountryLocal = {
+  _branch: "COUNTRY"
   id: string,
   idUbigeo: string,
   name: string,
@@ -26,3 +27,72 @@ export type Locality = {
   level: LocalityLevelType,
   parentId: string | null,
 }
+
+export type DepartmentLocal = {
+  _branch: "DEPARTMENT"
+  id: string,
+  idUbigeo: string,
+  name: string,
+  code: string,
+  tag: string,
+  searchValue: string,
+  level: LocalityLevelType,
+  parentId: string | null,
+}
+
+export type ProvinceLocal = {
+  _branch: "PROVINCE"
+  id: string,
+  idUbigeo: string,
+  name: string,
+  code: string,
+  tag: string,
+  searchValue: string,
+  level: LocalityLevelType,
+  parentId: string | null,
+}
+
+export type DisctrictLocal = {
+  _branch: "DISTRICT"
+  id: string,
+  idUbigeo: string,
+  name: string,
+  code: string,
+  tag: string,
+  searchValue: string,
+  level: LocalityLevelType,
+  parentId: string | null,
+}
+
+export type Locality = CountryLocal | DepartmentLocal | ProvinceLocal | DisctrictLocal
+
+export const CountryType = "COUNTRY";
+export type TypecountryType = typeof CountryType;
+
+export const DepartmentType = "DEPARTMENT";
+export type TypeDepartmentType = typeof DepartmentType;
+
+export const ProvinceType = "PROVINCE";
+export type TypeProvinceType = typeof ProvinceType;
+
+export const DistrictType = "DISTRICT";
+export type TypeDistrictType = typeof DistrictType;
+
+type LocalityLevelMap = {
+  [CountryType]: CountryLocal;
+  [DepartmentType]: DepartmentLocal;
+  [ProvinceType]: ProvinceLocal;
+  [DistrictType]: DisctrictLocal;
+}
+
+export type LocalityType = keyof LocalityLevelMap;
+
+export type InferLocalityType<T extends LocalityType | undefined> =
+  T extends LocalityType ? LocalityLevelMap[T] : Locality;
+
+export type GetManyParamsLocality<
+  T extends LocalityType | undefined = undefined,
+> = {
+  q?: string | null;
+  localityLevel: T;
+};
