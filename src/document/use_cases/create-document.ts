aff5c1d8929e18789d99fe4f11afdc110ba3a8bf
Jorge Gonzalez <jorg3.594@gmail.com>
2024-08-31 23:11:11 -0500

    Add document metadata logic to create-document.ts use case

--- src/document/use_cases/create-document.ts
+++ src/document/use_cases/create-document.ts
@@ -2,7 +2,13 @@
 
 import { response } from "@/lib/types";
 import { Order, OrderWithBusinessCustomer } from "@/order/types";
-import type { Document, Invoice, Receipt, Ticket } from "@/document/types";
+import type {
+  Document,
+  Invoice,
+  Receipt,
+  Ticket,
+  DocumentType,
+} from "@/document/types";
 import { hasBusinessCustomer } from "@/order/utils";
 
 export interface DocumentMetadata {
@@ -28,28 +34,49 @@ export interface DocumentGateway {
 
 interface Repository {
   createDocument: (document: Document) => Promise<response<Document>>;
+  getLastDocumentNumber: (serialNumber: string) => Promise<response<number>>;
+}
+
+interface BillingSettings {
+  invoiceSerialNumber: string;
+  receiptSerialNumber: string;
+  ticketSerialNumber: string;
+  establishmentCode: string;
 }
 
 export const createDocument = async (
   documentGateway: DocumentGateway,
   repository: Repository,
   order: Order,
+  billingConfig: BillingSettings,
 ): Promise<response<Document>> => {
   let documentResponse: response<Document>;
+  const documentNumberAndSerialResponse = await getDocumentNumberAndSerial(
+    billingConfig,
+    repository.getLastDocumentNumber,
+    order.documentType,
+  );
+  if (!documentNumberAndSerialResponse.success) {
+    return documentNumberAndSerialResponse;
+  }
+
+  const documentMetadata = {
+    ...documentNumberAndSerialResponse.data,
+    establishmentCode: billingConfig.establishmentCode,
+  };
+
   switch (order.documentType) {
     case "ticket":
-      documentResponse = await documentGateway.createTicket(order, {
-        serialNumber: "asdb",
-        documentNumber: 1,
-        establishmentCode: "4",
-      });
+      documentResponse = await documentGateway.createTicket(
+        order,
+        documentMetadata,
+      );
       break;
     case "receipt":
-      documentResponse = await documentGateway.createReceipt(order, {
-        serialNumber: "asdb",
-        documentNumber: 1,
-        establishmentCode: "4",
-      });
+      documentResponse = await documentGateway.createReceipt(
+        order,
+        documentMetadata,
+      );
       break;
     case "invoice":
       if (!hasBusinessCustomer(order)) {
@@ -58,11 +85,10 @@ export const createDocument = async (
           message: "Customer must be a BusinessCustomer",
         };
       }
-      documentResponse = await documentGateway.createInvoice(order, {
-        serialNumber: "asdb",
-        documentNumber: 1,
-        establishmentCode: "4",
-      });
+      documentResponse = await documentGateway.createInvoice(
+        order,
+        documentMetadata,
+      );
       break;
     default:
       throw new Error("Invalid document type");
@@ -73,4 +99,39 @@ export const createDocument = async (
   }
 
   return await repository.createDocument(documentResponse.data);
+};
+
+const getDocumentNumberAndSerial = async (
+  billingSettings: BillingSettings,
+  obtainLastNumber: Repository["getLastDocumentNumber"],
+  documentType: DocumentType,
+): Promise<response<{ documentNumber: number; serialNumber: string }>> => {
+  let response: response<number>;
+  let serialNumber: string;
+
+  switch (documentType) {
+    case "invoice":
+      response = await obtainLastNumber(billingSettings.invoiceSerialNumber);
+      serialNumber = billingSettings.invoiceSerialNumber;
+      break;
+    case "receipt":
+      response = await obtainLastNumber(billingSettings.receiptSerialNumber);
+      serialNumber = billingSettings.receiptSerialNumber;
+      break;
+    case "ticket":
+      response = await obtainLastNumber(billingSettings.ticketSerialNumber);
+      serialNumber = billingSettings.ticketSerialNumber;
+      break;
+    default:
+      throw new Error("Invalid document type");
+  }
+
+  if (!response.success) {
+    return response;
+  }
+
+  return {
+    success: true,
+    data: { documentNumber: response.data + 1, serialNumber: serialNumber },
+  };
 };
