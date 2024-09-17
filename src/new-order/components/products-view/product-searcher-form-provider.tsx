ecc326bc8f562f26a9f552d2aced42f43be00ba0
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-09-17 18:01:23 -0500

    Create store for product

---
+++ src/new-order/components/products-view/product-searcher-form-provider.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import {createContext, type ReactNode, useContext, useRef} from "react";
+import {StoreApi, useStore} from "zustand";
+import {
+  Actions,
+  createProductFormStore,
+  initProductFormStore, ProductFormStore
+} from "@/new-order/components/products-view/store-products-searcher";
+import {Product} from "@/product/types";
+
+const ProductFormStoreContext = createContext<StoreApi<ProductFormStore> | null>(
+  null,
+);
+
+interface ProductFormProviderProps {
+  children: ReactNode;
+}
+
+export const ProductFormProvider = ({ children }: ProductFormProviderProps) => {
+  const storeRef = useRef<StoreApi<ProductFormStore>>();
+  if (!storeRef.current) {
+    storeRef.current = createProductFormStore(initProductFormStore());
+  }
+
+  return (
+    <ProductFormStoreContext.Provider value={storeRef.current}>
+      {children}
+    </ProductFormStoreContext.Provider>
+  );
+};
+
+export const useProductFormStore = <T,>(
+  selector: (store: ProductFormStore) => T,
+): T => {
+  const productFormStoreContext = useContext(ProductFormStoreContext);
+
+  if (!productFormStoreContext) {
+    throw new Error(
+      "useOrderFormStore must be used within a ProductFormProvider",
+    );
+  }
+
+  return useStore(productFormStoreContext, selector);
+};
+
+export const useProductFormActions = (): Actions => {
+
+  const productFormStoreContext = useContext(ProductFormStoreContext);
+  if (!productFormStoreContext) {
+    throw new Error(
+      "useOrderFormStore must be used within a OrderFormProvider",
+    );
+  }
+
+  const setProducts = (productsStore: Product[]) => {
+    productFormStoreContext.setState(() => {
+      return { products: productsStore };
+    });
+  };
+
+  const increaseQuantityProduct = (productId: string) => {
+    const { products } = productFormStoreContext.getState();
+    const product = products.find((product) => product.id === productId);
+      console.log(productId)
+    if (!product) {
+      console.error("Product item not found");
+      return;
+    }
+
+    if(product?.type === "SingleProduct"){
+      product.stock+= 1;
+      productFormStoreContext.setState(() => {
+        return { products: products.map(p => p.id === productId ? product : p) };
+      });
+    }
+  }
+
+  const decreaseQuantityProduct = (productId: string) => {
+    const { products } = productFormStoreContext.getState();
+    const product = products.find((product) => product.id === productId);
+    if (!product) {
+      console.error("Product item not found");
+      return;
+    }
+
+    if(product?.type === "SingleProduct"){
+      if(product.stock >= 1)product.stock--
+      productFormStoreContext.setState(() => {
+        return { products: products.map(p => p.id === productId ? product : p) };
+      });
+    }
+  }
+
+  return {
+    setProducts,
+    decreaseQuantityProduct,
+    increaseQuantityProduct,
+  }
+}
---
+++ src/new-order/components/products-view/store-products-searcher.tsx
@@ -0,0 +1,33 @@
+import {Product} from "@/product/types";
+import {createStore} from "zustand/vanilla";
+
+
+export type ProductFormStore = {
+  products: Product[];
+}
+
+export type Actions = {
+  setProducts: (products: Product[]) => void;
+  increaseQuantityProduct: (productId: string) => void;
+  decreaseQuantityProduct: (productId: string) => void;
+}
+
+const defaultInitState: ProductFormStore = {
+  products: [],
+}
+
+export const initProductFormStore = (): ProductFormStore => {
+  return {
+    ...defaultInitState,
+    products: { ...defaultInitState.products},
+  };
+};
+
+// The actions are set on the context provider
+export const createProductFormStore = (
+  initState: ProductFormStore = defaultInitState,
+) => {
+  return createStore<ProductFormStore>()(() => ({
+    ...initState,
+  }));
+};
