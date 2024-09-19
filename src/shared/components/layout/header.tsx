8eda22fba04eee18277a15dfaa8d5e91664618e7
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-09-19 03:29:58 -0500

    Use logo component in the header

--- src/shared/components/layout/header.tsx
+++ src/shared/components/layout/header.tsx
@@ -1,18 +1,20 @@
 import { cn } from "@/lib/utils";
 import { MobileSidebar } from "./mobile-sidebar";
 import { UserNav } from "./user-nav";
-import Link from "next/link";
+import {LogoImage} from "@/shared/components/layout/logo";
 
-export default function Header() {
+export default async function Header() {
   return (
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className={cn("block lg:!hidden")}>
           <MobileSidebar />
         </div>
-
+        <div className="mt-4 mx-auto relative w-[100px] h-[100px] rounded-md overflow-hidden">
+          <LogoImage/>
+        </div>
         <div className="flex items-center justify-end w-full">
-          <UserNav />
+          <UserNav/>
         </div>
       </nav>
     </div>
