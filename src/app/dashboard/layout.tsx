4ea25070223eaad403193a90f1a3039dbc0e3981
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-09-19 02:26:46 -0500

    Use store globlal to logo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
-import { OrderFormProvider } from "@/new-order/order-form-provider";
-import { CategoryStoreProvider } from "@/category/components/category-store-provider";
-import { CashShiftStoreProvider } from "@/cash-shift/components/cash-shift-store-provider";
+import {OrderFormProvider} from "@/new-order/order-form-provider";
+import {CategoryStoreProvider} from "@/category/components/category-store-provider";
+import {CashShiftStoreProvider} from "@/cash-shift/components/cash-shift-store-provider";
 import CategoriesLoader from "@/category/components/categories-loader";
-import { ProductsStoreProvider } from "@/product/components/products-store-provider";
-import { AdvertenceModal } from "@/shared/components/modal/advertence-modal";
+import {ProductsStoreProvider} from "@/product/components/products-store-provider";
+import {AdvertenceModal} from "@/shared/components/modal/advertence-modal";
+import {LogoStoreProvider} from "@/company/logo-store-provider";
 
 export default function DashboardLayout({
   children,
@@ -12,13 +13,15 @@ export default function DashboardLayout({
 }) {
   return (
     <OrderFormProvider>
-      <CategoryStoreProvider>
-        <ProductsStoreProvider>
-          <CashShiftStoreProvider>
-            <CategoriesLoader>{children}</CategoriesLoader>
-          </CashShiftStoreProvider>
-        </ProductsStoreProvider>
-      </CategoryStoreProvider>
+      <LogoStoreProvider>
+        <CategoryStoreProvider>
+          <ProductsStoreProvider>
+            <CashShiftStoreProvider>
+              <CategoriesLoader>{children}</CategoriesLoader>
+            </CashShiftStoreProvider>
+          </ProductsStoreProvider>
+        </CategoryStoreProvider>
+      </LogoStoreProvider>
     </OrderFormProvider>
   );
 }
