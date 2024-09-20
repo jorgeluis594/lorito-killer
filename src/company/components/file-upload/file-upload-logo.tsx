cb50d8bd5a4243a7c75ebc25d9e90e49be24e629
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-09-20 04:56:01 -0500

    Refactor upload logo

--- src/company/components/file-upload/file-upload-logo.tsx
+++ src/company/components/file-upload/file-upload-logo.tsx
@@ -28,63 +28,61 @@ export default function LogoUpload({
 
   return (
     <div>
-      <div>
-          <UploadDropzone
-            className="dark:bg-zinc-800 py-2 ut-label:text-sm ut-allowed-content:ut-uploading:text-red-300"
-            endpoint="imageUploader"
-            config={{mode: "auto"}}
-            content={{
-              allowedContent({isUploading}) {
-                return isUploading ? "Subiendo imagen" : "Máximo imagenes de 4mb"
-              },
-              label() {
-                return "Arrastra y suelta o haz clic para subir tu Logo";
-              }
-            }}
-            onClientUploadComplete={(res: any | undefined) => {
-              console.log(res)
-              if (res) {
-                onUpdateFile(res[0]);
-              }
-            }}
-            onUploadError={(error: Error) => {
-              toast({
-                title: "Error",
-                variant: "destructive",
-                description: error.message,
-              });
-            }}
-            onUploadBegin={(name: string) => {
-            }}
-          />
-      </div>
-      <div className="my-4 flex items-center justify-center gap-4">
-        {!!value && (
-            <div
-              key={value.key}
-              className="relative w-[180px] h-[180px] rounded-md overflow-hidden"
-            >
-              <div className="z-10 absolute top-2 right-2">
-                <Button
-                  type="button"
-                  onClick={() => onDeleteFile()}
-                  variant="destructive"
-                  size="sm"
-                >
-                  <Trash className="h-4 w-4"/>
-                </Button>
-              </div>
-              <div>
-                <Image
-                  fill
-                  className="object-cover item"
-                  alt="Image"
-                  src={value.url || ""}
-                />
-              </div>
+      {!value && ( // Solo mostrar el UploadDropzone si no hay imagen cargada
+        <UploadDropzone
+          className="dark:bg-zinc-800 py-2 ut-label:text-sm ut-allowed-content:ut-uploading:text-red-300"
+          endpoint="imageUploader"
+          config={{ mode: "auto" }}
+          content={{
+            allowedContent({ isUploading }) {
+              return isUploading ? "Subiendo imagen" : "Máximo imagenes de 4mb";
+            },
+            label() {
+              return "Arrastra y suelta o haz clic para subir tu Logo";
+            },
+          }}
+          onClientUploadComplete={(res: any | undefined) => {
+            console.log(res);
+            if (res) {
+              onUpdateFile(res[0]);
+            }
+          }}
+          onUploadError={(error: Error) => {
+            toast({
+              title: "Error",
+              variant: "destructive",
+              description: error.message,
+            });
+          }}
+          onUploadBegin={(name: string) => {}}
+        />
+      )}
+
+      {value && ( // Mostrar la imagen solo si hay un valor
+        <div className="my-4 flex items-center justify-center gap-4">
+          <div
+            key={value.key}
+            className="relative w-[180px] h-[180px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={onDeleteFile}
+                variant="destructive"
+                size="sm"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
             </div>
-        )}
-      </div>
+            <Image
+              fill
+              className="object-cover item"
+              alt="Image"
+              src={value.url || ""}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
