0541228c0a388e7f90972396d2f2d5deb013fd2d
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-09-29 02:29:15 -0500

    Add logo for home page

--- src/app/[subdomain]/login/page.tsx
+++ src/app/[subdomain]/login/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next";
-import Link from "next/link";
 import UserAuthForm from "@/shared/user-auth-form";
-import { buttonVariants } from "@/shared/components/ui/button";
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -13,22 +11,7 @@ export default function AuthenticationPage() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
-        </div>
+          <Image fill className="object-cover" alt="Logo" src="/kogoz.svg" />
       </div>
       <div className="p-4 lg:p-8 h-full flex items-center">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
