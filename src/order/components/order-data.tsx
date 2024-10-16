6151ac4fdbce4f2f3bce89b8f68664bd3e8f93e2
Jorge Gonzalez <jorg3.594@gmail.com>
2024-10-16 10:18:22 -0500

    Refactor order data component to be used from server side

--- src/order/components/order-data.tsx
+++ src/order/components/order-data.tsx
@@ -8,48 +8,21 @@ import {
 import { formatPrice, localizeDate, paymentMethodToText } from "@/lib/utils";
 import { Printer } from "lucide-react";
 import { buttonVariants } from "@/shared/components/ui/button";
-import { Company } from "@/company/types";
 import { UNIT_TYPE_MAPPER } from "@/product/constants";
-import {findCustomerById} from "@/customer/actions";
-import {useEffect, useState} from "react";
-import {Customer} from "@/customer/types";
-import {fullName} from "@/customer/utils";
+import { fullName } from "@/customer/utils";
 
-export default function OrderData({
-                                    order,
-                                    company,
-                                  }: {
-  order: Order;
-  company: Company;
-}) {
-  const [customerResponse, setCustomerResponse] = useState<Customer>();
-  const [customerFullName, setCustomerFullName] = useState<string>("");
-
-  useEffect(() => {
-    const fetchCustomerData = async () => {
-      const response = await findCustomerById(order.customerId);
-      if(!response.success) { return response}
-      setCustomerResponse(response.data);
-    };
-
-    fetchCustomerData();
-  }, [order.customerId]);
-
-  useEffect(() => {
-    if (customerResponse) {
-      const nameCustomer = fullName(customerResponse);
-      setCustomerFullName(nameCustomer);
-    }
-  }, [customerResponse]);
-
+export default function OrderData({ order }: { order: Order }) {
   return (
     <div className="h-full mt-8 flex justify-center">
       <Card className={"w-11/12"}>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 mb-4">
           <CardTitle>
-            {order.documentType === "receipt" ? "Boleta Electrónica" :
-              order.documentType === "invoice" ? "Factura Electrónica" :
-                "Nota de venta"} ({order.id!.substring(0, 8)})
+            {order.documentType === "receipt"
+              ? "Boleta Electrónica"
+              : order.documentType === "invoice"
+                ? "Factura Electrónica"
+                : "Nota de venta"}{" "}
+            ({order.id!.substring(0, 8)})
           </CardTitle>
           <a
             className={buttonVariants({ variant: "ghost" })}
@@ -63,95 +36,98 @@ export default function OrderData({
         <CardContent>
           <table className="table-auto border w-full">
             <tbody>
-            <tr>
-              <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
-                Cliente
-              </td>
-              <td className="pl-2 border py-1">
-                { customerResponse ? customerFullName : 'Cliente General'
-                }
-              </td>
-            </tr>
-            <tr>
-              <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
-                Email
-              </td>
-              <td className="pl-2 border py-1">{customerResponse?.email || "No disponible"}</td>
-            </tr>
-            <tr>
-              <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
-                Teléfono
-              </td>
-              <td className="pl-2 border py-1">{customerResponse?.phoneNumber || 'No disponible'}</td>
-            </tr>
-            <tr>
-              <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
-                Fecha de compra
-              </td>
-              <td className="pl-2 border py-1">
-                {localizeDate(order.createdAt!)}
-              </td>
-            </tr>
+              <tr>
+                <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
+                  Cliente
+                </td>
+                <td className="pl-2 border py-1">
+                  {order.customer ? fullName(order.customer) : "-"}
+                </td>
+              </tr>
+              <tr>
+                <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
+                  Email
+                </td>
+                <td className="pl-2 border py-1">
+                  {order.customer?.email || "No disponible"}
+                </td>
+              </tr>
+              <tr>
+                <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
+                  Teléfono
+                </td>
+                <td className="pl-2 border py-1">
+                  {order.customer?.phoneNumber || "No disponible"}
+                </td>
+              </tr>
+              <tr>
+                <td className="pl-2 border w-13 py-1 bg-slate-100 font-light w-56">
+                  Fecha de compra
+                </td>
+                <td className="pl-2 border py-1">
+                  {localizeDate(order.createdAt!)}
+                </td>
+              </tr>
             </tbody>
           </table>
 
           <table className="table-auto border w-full mt-8">
             <thead>
-            <tr>
-              <th className="bg-slate-100 pl-2 border py-1 font-light">
-                Producto
-              </th>
-              <th className="bg-slate-100 pl-2 border py-1 font-light">
-                Precio
-              </th>
-              <th className="bg-slate-100 pl-2 border py-1 font-light">
-                Cantidad
-              </th>
-              <th className="bg-slate-100 pl-2 border py-1 font-light">
-                Total
-              </th>
-            </tr>
+              <tr>
+                <th className="bg-slate-100 pl-2 border py-1 font-light">
+                  Producto
+                </th>
+                <th className="bg-slate-100 pl-2 border py-1 font-light">
+                  Precio
+                </th>
+                <th className="bg-slate-100 pl-2 border py-1 font-light">
+                  Cantidad
+                </th>
+                <th className="bg-slate-100 pl-2 border py-1 font-light">
+                  Total
+                </th>
+              </tr>
             </thead>
             <tbody>
-            {order.orderItems.map((orderItem) => (
-              <tr key={orderItem.id}>
-                <td className="pl-2 border py-1">{orderItem.productName}</td>
-                <td className="pl-2 border py-1">
-                  {formatPrice(orderItem.productPrice)}
-                </td>
-                <td className="pl-2 border py-1">
-                  {orderItem.quantity} {UNIT_TYPE_MAPPER[orderItem.unitType]}
-                </td>
-                <td className="pl-2 border py-1">
-                  {formatPrice(orderItem.total)}
-                </td>
-              </tr>
-            ))}
+              {order.orderItems.map((orderItem) => (
+                <tr key={orderItem.id}>
+                  <td className="pl-2 border py-1">{orderItem.productName}</td>
+                  <td className="pl-2 border py-1">
+                    {formatPrice(orderItem.productPrice)}
+                  </td>
+                  <td className="pl-2 border py-1">
+                    {orderItem.quantity} {UNIT_TYPE_MAPPER[orderItem.unitType]}
+                  </td>
+                  <td className="pl-2 border py-1">
+                    {formatPrice(orderItem.total)}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
           <div className="w-full flex justify-between mt-8">
             <table className="table-auto border w-64">
               <thead>
-              <tr>
-                <th className="bg-slate-100 pl-2 border py-1 font-light">
-                  Método de pago
-                </th>
-                <th className="bg-slate-100 pl-2 border py-1 font-light">
-                  Monto
-                </th>
-              </tr>
+                <tr>
+                  <th className="bg-slate-100 pl-2 border py-1 font-light">
+                    Método de pago
+                  </th>
+                  <th className="bg-slate-100 pl-2 border py-1 font-light">
+                    Monto
+                  </th>
+                </tr>
               </thead>
               <tbody>
-              {order.payments.map((payment) => (
-                <tr key={payment.id}>
-                  <td className="pl-2 border py-1">
-                    {paymentMethodToText(payment.method)}
-                  </td>
-                  <td className="pl-2 border py-1">
-                    {formatPrice(payment.amount)}
-                  </td>
-                </tr>
-              ))}
+                {order.payments.map((payment) => (
+                  <tr key={payment.id}>
+                    <td className="pl-2 border py-1">
+                      {paymentMethodToText(payment.method)}
+                    </td>
+                    <td className="pl-2 border py-1">
+                      {formatPrice(payment.amount)}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
             <div className="w-56 flex justify-between">
--- src/order/components/order-item.tsx
+++ src/order/components/order-item.tsx
@@ -2,27 +2,31 @@
 
 import { Order } from "@/order/types";
 import { formatPrice, localizeDate } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface OrderItemProps {
   order: Order;
-  isCurrent: boolean;
-  onSelect: (order: Order) => void;
 }
 
-export default function OrderItem({
-  order,
-  onSelect,
-  isCurrent,
-}: OrderItemProps) {
+export default function OrderItem({ order }: OrderItemProps) {
+  const path = usePathname();
+  const href = `/dashboard/orders/${order.id}`;
+
   return (
-    <div
-      className={`flex items-center justify-between p-4 border-b cursor-pointer hover:bg-gray-100 transition-colors ${isCurrent ? "bg-gray-100" : ""}`}
-      onClick={() => onSelect(order)}
+    <Link
+      href={href}
+      className={`flex items-center justify-between p-4 border-b cursor-pointer hover:bg-gray-100 transition-colors ${path == href ? "bg-gray-100" : ""}`}
     >
       <div>
-        <div className="text-sm font-medium">{order.documentType === "receipt" ? "Boleta Electrónica" :
-          order.documentType === "invoice" ? "Factura Electrónica" :
-            "Nota de venta"} ({order.id!.substring(0, 8)})</div>
+        <div className="text-sm font-medium">
+          {order.documentType === "receipt"
+            ? "Boleta Electrónica"
+            : order.documentType === "invoice"
+              ? "Factura Electrónica"
+              : "Nota de venta"}{" "}
+          ({order.id!.substring(0, 8)})
+        </div>
         <div className="text-xs text-gray-500">
           {localizeDate(order.createdAt!)}
         </div>
@@ -31,6 +35,6 @@ export default function OrderItem({
         <div className="text-sm font-medium">{formatPrice(order.total)}</div>
         <div className="text-xs text-gray-500 text-end">Pagado</div>
       </div>
-    </div>
+    </Link>
   );
 }
