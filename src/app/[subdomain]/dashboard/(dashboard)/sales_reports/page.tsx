90e61ace2d7a3fb31513086dc090fa4f1aa6fa81
Jorge Gonzalez <jorg3.594@gmail.com>
2024-10-26 10:16:36 -0500

    Add sales reports page

---
+++ src/app/[subdomain]/dashboard/(dashboard)/sales_reports/page.tsx
@@ -0,0 +1,101 @@
+import BreadCrumb from "@/shared/breadcrumb";
+import { Heading } from "@/shared/components/ui/heading";
+import { Separator } from "@/shared/components/ui/separator";
+import DataTable from "@/sale_report/components/table/client";
+import { columns } from "@/sale_report/components/table/columns";
+import { getSession } from "@/lib/auth";
+import { Button } from "@/shared/components/ui/button";
+import { Download } from "lucide-react";
+import { getMany } from "@/document/db_repository";
+import { SearchParams } from "@/document/types";
+import { Suspense } from "react";
+
+const breadcrumbItems = [
+  { title: "Reporte de ventas", link: "/sales_reports" },
+];
+
+type ParamsProps = {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+};
+
+const getSearchParams = async ({
+  searchParams,
+}: ParamsProps): Promise<SearchParams> => {
+  const session = await getSession();
+
+  const params: SearchParams = {
+    companyId: session.user.companyId,
+    pageNumber: Number(searchParams.page) || 1,
+    pageSize: Number(searchParams.size) || 10,
+  };
+
+  if (searchParams.series && searchParams.number) {
+    params.correlative = {
+      number: searchParams.number as string,
+      series: searchParams.series as string,
+    };
+  }
+
+  if (searchParams.start) {
+    params.startDate = new Date(searchParams.start as string);
+  }
+
+  if (searchParams.end) {
+    params.endDate = new Date(searchParams.end as string);
+  }
+
+  if (searchParams.customerId) {
+    params.customerId = searchParams.customerId as string;
+  }
+
+  return params;
+};
+
+async function DocumentsWithSuspense({ searchParams }: ParamsProps) {
+  const documentsResponse = await getMany(
+    await getSearchParams({ searchParams }),
+  );
+
+  if (!documentsResponse.success) {
+    return <p>Error cargando los documentos, comuniquese con soporte</p>;
+  }
+
+  return (
+    <DataTable
+      data={documentsResponse.data}
+      columns={columns}
+      pageCount={Number(searchParams.page) || 1}
+    />
+  );
+}
+
+export default async function Page({ searchParams }: ParamsProps) {
+  return (
+    <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
+      <BreadCrumb items={breadcrumbItems} />
+
+      <div className="flex items-start justify-between">
+        <Heading title="Reporte de ventas" />
+      </div>
+      <Separator />
+      <div className="flex flex-row space-x-12 space-y-0 mt-8">
+        <aside className="w-1/5 space-y-2">
+          <Button type="button">
+            <Download className="h-4 w-4 mr-2" />
+            Exportar
+          </Button>
+          <p>Aca van los filtros</p>
+        </aside>
+        <div className="flex-1 lg:max-w-7xl mt-6">
+          <Suspense
+            fallback={<DataTable loading columns={columns} pageCount={1} />}
+          >
+            <DocumentsWithSuspense searchParams={searchParams} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+}
