92a77d88e411d6de2134277d973d15f952a27820
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2024-10-29 05:42:08 -0500

    Add scroll in order list

--- src/app/[subdomain]/dashboard/orders/(index)/layout.tsx
+++ src/app/[subdomain]/dashboard/orders/(index)/layout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { getLastOpenCashShift } from "@/cash-shift/db_repository";
 import { getSession } from "@/lib/auth";
 import OrderItem from "@/order/components/order-item";
+import {NextSSRPlugin} from "@uploadthing/react/next-ssr-plugin";
+import {extractRouterConfig} from "uploadthing/server";
+import {ourFileRouter} from "@/app/api/uploadthing/core";
+import {ScrollArea} from "@/shared/components/ui/scroll-area";
 
 export default async function OrdersIndexLayout({
   children,
@@ -14,12 +18,14 @@ export default async function OrdersIndexLayout({
   return (
     <div className="h-[calc(100vh-theme(space.14))]">
       <div className="grid grid-cols-[380px_1fr] h-full">
-        <div className="h-full border-r">
-          {cashShiftResponse.success &&
-            cashShiftResponse.data.orders.map((order) => (
-              <OrderItem key={order.id} order={order} />
-            ))}
-        </div>
+        <ScrollArea className="h-full">
+          <div className="h-full border-r">
+            {cashShiftResponse.success &&
+              cashShiftResponse.data.orders.map((order) => (
+                <OrderItem key={order.id} order={order}/>
+              ))}
+          </div>
+        </ScrollArea>
         <div className="h-full">{children}</div>
       </div>
     </div>
