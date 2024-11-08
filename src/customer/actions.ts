0a15a228ac89a9e69c39845def754f3f9f2a623a
Jorge Gonzalez <jorg3.594@gmail.com>
2024-11-08 18:26:26 -0500

    log search customer

--- src/customer/actions.ts
+++ src/customer/actions.ts
@@ -9,18 +9,21 @@ import {
 import gatewayCreator from "@/document/factpro/gateway";
 import { getSession } from "@/lib/auth";
 import { getBillingCredentialsFor } from "@/document/db_repository";
-import {log} from "@/lib/log";
+import { log } from "@/lib/log";
 
 export const createCustomer = async (
   customer: Customer,
 ): Promise<response<Customer>> => {
   const session = await getSession();
 
-  const responseFind = await findByDocumentNumber(session.user.companyId, String(customer.documentNumber))
+  const responseFind = await findByDocumentNumber(
+    session.user.companyId,
+    String(customer.documentNumber),
+  );
 
-  if(responseFind.success){
-    log.info("Customer is already registered", {customer, responseFind})
-    return {success: false, message: "Cliente ya registrado."}
+  if (responseFind.success) {
+    log.info("Customer is already registered", { customer, responseFind });
+    return { success: false, message: "Cliente ya registrado." };
   }
 
   return persistCustomer({ ...customer, companyId: session.user.companyId });
@@ -38,11 +41,17 @@ export const searchCustomer = async (
     return billingCredentialsResponse;
   }
 
-  const responseFind = await findByDocumentNumber(session.user.companyId, documentNumber)
+  const responseFind = await findByDocumentNumber(
+    session.user.companyId,
+    documentNumber,
+  );
 
-  if(responseFind.success){
-    log.info("Customer is already registered", {documentNumber, responseFind})
-    return {success: false, message: "Cliente ya registrado."}
+  if (responseFind.success) {
+    log.info("Customer is already registered", {
+      documentNumber,
+      responseFind,
+    });
+    return { success: false, message: "Cliente ya registrado." };
   }
 
   const { fetchCustomerByRuc, fetchCustomerByDNI } = gatewayCreator({
@@ -55,6 +64,7 @@ export const searchCustomer = async (
   const response = await fetchFunction(documentNumber);
 
   if (!response.success) {
+    log.error("fetch_customer_failed", { documentNumber, response });
     return { success: false, message: "No se encontro al cliente" };
   }
 
