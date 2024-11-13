f950e2986abbc8b7c09527357f4b61543d1e7f3b
Jorge Gonzalez <jorg3.594@gmail.com>
2024-11-13 06:51:54 -0500

    Improve card display

--- src/new-order/components/products-view/product-item.tsx
+++ src/new-order/components/products-view/product-item.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { useOrderFormActions } from "@/new-order/order-form-provider";
 import { useState } from "react";
 import KgCalculatorForm from "@/new-order/components/cart/kg-calculator-form";
-import {useProductFormActions} from "@/new-order/components/products-view/product-searcher-form-provider";
+import { useProductFormActions } from "@/new-order/components/products-view/product-searcher-form-provider";
 
 export default function ProductItem({ product }: { product: Product }) {
   const photoUrl = product.photos![0]?.url || "";
@@ -16,7 +16,7 @@ export default function ProductItem({ product }: { product: Product }) {
   const { addProduct, updateOrderItem, getOrderItemByProduct } =
     useOrderFormActions();
 
-  const { decreaseQuantityProduct } = useProductFormActions()
+  const { decreaseQuantityProduct } = useProductFormActions();
 
   const onAddProductToCart = () => {
     if (
@@ -28,7 +28,7 @@ export default function ProductItem({ product }: { product: Product }) {
     }
 
     addProduct(product);
-    decreaseQuantityProduct(product.id!)
+    decreaseQuantityProduct(product.id!);
   };
 
   const onKgCalculatorSubmit = (kg: number) => {
@@ -48,7 +48,7 @@ export default function ProductItem({ product }: { product: Product }) {
   };
 
   return (
-    <>
+    <div className="relative">
       {openKgCalculator && (
         <KgCalculatorForm
           open={openKgCalculator}
@@ -58,15 +58,18 @@ export default function ProductItem({ product }: { product: Product }) {
           onSubmit={onKgCalculatorSubmit}
         />
       )}
-      <Card onClick={onAddProductToCart}>
-        <CardContent className="px-4">
+      <Card
+        onClick={onAddProductToCart}
+        className="hover:absolute hover:h-auto hover:z-10 hover:max-h-none max-h-full group hover:shadow-xl transition duration-300"
+      >
+        <CardContent className="px-2">
           <div className="mt-4 mx-auto relative w-[100px] h-[100px] rounded-md overflow-hidden">
             <Image fill className="object-cover" alt="Image" src={photoUrl} />
           </div>
-          <p className="text-lg text-center font-light leading-none mt-8">
+          <p className="text-center font-light leading-4 mt-2 line-clamp-4 group-hover:line-clamp-none">
             {product.name}
           </p>
-          <p className="text-lg text-center font-medium mt-2">
+          <p className="text-lg text-center font-medium my-1">
             {formatPrice(product.price)}
           </p>
           <p className="text-sm text-center mt-1">
@@ -76,6 +79,6 @@ export default function ProductItem({ product }: { product: Product }) {
           </p>
         </CardContent>
       </Card>
-    </>
+    </div>
   );
 }
