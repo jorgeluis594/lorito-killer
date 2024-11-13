cfda20f8fa5312d9179d4848611e166a5230f924
Jorge Gonzalez <jorg3.594@gmail.com>
2024-11-13 06:19:35 -0500

    Set company location attributes null

--- prisma/migrations/20241105225316_net_total_set_null/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `netTotal` on table `Document` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Document" ALTER COLUMN "netTotal" SET NOT NULL;
