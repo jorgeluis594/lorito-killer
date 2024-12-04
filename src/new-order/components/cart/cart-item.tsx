879fa4fd9ec2e6e080e8bd412bca730222506337
Jorge Gonzalez <jorg3.594@gmail.com>
2024-12-03 22:12:17 -0500

    Styles improvements on cart-item.tsx

--- src/new-order/components/cart/cart-item.tsx
+++ src/new-order/components/cart/cart-item.tsx
@@ -2,7 +2,7 @@ import { OrderItem } from "@/order/types";
 import { Plus, Minus } from "lucide-react";
 import { formatPrice } from "@/lib/utils";
 import { Button } from "@/shared/components/ui/button";
-import { Trash } from "lucide-react";
+import { Trash2 } from "lucide-react";
 import KgQuantity from "@/new-order/components/cart/kg-quantity";
 
 interface CartItemProps {
@@ -25,18 +25,18 @@ export default function CartItem({
   restoreStockProduct,
 }: CartItemProps) {
   const onIncreaseIncreaseQuantity = () => {
-    increaseQuantity(item.id!)
-    decreaseQuantityProduct(item.productId)
+    increaseQuantity(item.id!);
+    decreaseQuantityProduct(item.productId);
   };
   const onDecreaseIncreaseQuantity = () => {
-    decreaseQuantity(item.id!)
-    increaseQuantityProduct(item.productId)
+    decreaseQuantity(item.id!);
+    increaseQuantityProduct(item.productId);
   };
 
   const removeProductToCart = () => {
-    removeOrderItem(item.id!)
-    restoreStockProduct(item.productId, item.quantity)
-  }
+    removeOrderItem(item.id!);
+    restoreStockProduct(item.productId, item.quantity);
+  };
 
   const UnitQuantityComponent = () => (
     <div className="flex justify-around items-center">
@@ -51,8 +51,8 @@ export default function CartItem({
   );
 
   return (
-    <div className="py-2 border-b grid grid-cols-[270px,1fr,140px] transition animate-move-from-left-to-right hover:bg-accent group">
-      <div className="pl-4 ">
+    <div className="py-2 px-4 border-b grid grid-cols-[270px,1fr,140px] transition animate-move-from-left-to-right hover:bg-accent group">
+      <div>
         <p className="text-sm">{item.productName}</p>
         <p className="text-sm text-muted-foreground">
           {formatPrice(item.productPrice)}
@@ -67,14 +67,16 @@ export default function CartItem({
         <p className="text-end text-xl font-medium group-hover:hidden">
           {formatPrice(item.total)}
         </p>
-        <Button
-          type="button"
-          variant="destructive"
-          className="hidden group-hover:block ml-auto"
-          onClick={removeProductToCart}
-        >
-          <Trash className="h-4 w-4"></Trash>
-        </Button>
+        <div className="hidden group-hover:flex justify-end space-x-2">
+          <Button
+            type="button"
+            size="icon"
+            variant="destructive"
+            onClick={removeProductToCart}
+          >
+            <Trash2 />
+          </Button>
+        </div>
       </div>
     </div>
   );
