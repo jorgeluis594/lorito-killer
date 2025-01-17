7f562f97fbe7f4b87a47ee2c8410e15d09e4bd54
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-01-17 06:39:06 -0500

    Add function to search for best selling product in the db_repository from dashboard entity

--- src/sales-dashboard/db_repository.ts
+++ src/sales-dashboard/db_repository.ts
@@ -1,7 +1,9 @@
 import {response} from "@/lib/types";
 import prisma from "@/lib/prisma";
-import {Sales} from "@/sales-dashboard/type";
+import {ProductToSales, Sales} from "@/sales-dashboard/type";
 import {plus} from "@/lib/utils";
+import {log} from "@/lib/log";
+import {OrderItem} from "@/order/types";
 
 export const findSales = async (companyId: string, startOfMonth: Date, endOfMonth: Date): Promise<response<Sales>> => {
   const salesFound = (
@@ -16,4 +18,51 @@ export const findSales = async (companyId: string, startOfMonth: Date, endOfMont
   }, 0);
 
   return {success:true, data: {finalAmount: totalSales}}
+}
+
+export const findProductToSales = async (): Promise<response<boolean>> => {
+
+  const orderItems = await prisma().orderItem.groupBy({
+    by: ['productId'],
+    _sum: {
+      quantity: true,
+    },
+    orderBy: {
+      _sum: {
+        quantity: 'desc',
+      },
+    },
+    take: 10,
+  });
+
+  const productIds = orderItems.map(item => item.productId);
+
+  const products = await prisma().product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const productData = products.map(product => ({
+    productId: product.id,
+    name: product.name,
+    price: product.price,
+  }));
+
+  const productTotals = orderItems.map(item => {
+    const product = productData.find(product => product.productId === item.productId);
+    const quantity = item._sum.quantity || 0
+    const price = product?.price || 0
+    const totalPrice = +price * +quantity;
+    return {
+      name: product?.name,
+      total: totalPrice,
+    };
+  });
+
+  log.info("productInfo",{orderItems,productData, productTotals})
+
+  return {success: false, message: "uwu"};
 }
