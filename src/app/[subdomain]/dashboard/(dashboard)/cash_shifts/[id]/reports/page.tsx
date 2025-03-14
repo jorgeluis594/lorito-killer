efe1effe94ac56a74a26653cfa530d1ae91d6e2b
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-14 07:09:26 -0500

    Add utility call in the page.tsx to report

--- src/app/[subdomain]/dashboard/(dashboard)/cash_shifts/[id]/reports/page.tsx
+++ src/app/[subdomain]/dashboard/(dashboard)/cash_shifts/[id]/reports/page.tsx
@@ -5,6 +5,7 @@ import CashShiftReportTw from "@/cash-shift/cash-shift-report-tailwind";
 import { findCashShift } from "@/cash-shift/db_repository";
 import { notFound } from "next/navigation";
 import { CashShift } from "@/cash-shift/types";
+import {findUtility} from "@/cash-shift/components/actions";
 
 const breadcrumbItems = [
   { title: "Caja chica", link: "/cash_shifts" },
@@ -21,6 +22,11 @@ export default async function Page({
     return notFound();
   }
 
+  const grossProfitResponse = await findUtility(id)
+  if (!grossProfitResponse.success) {
+    return notFound();
+  }
+
   return (
     <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
       <BreadCrumb items={breadcrumbItems} />
@@ -29,7 +35,7 @@ export default async function Page({
       </div>
       <Separator />
       <div className="h-[calc(100vh-theme(space.64))]">
-        <CashShiftReportTw cashShift={cashShiftFoundResponse.data} />
+        <CashShiftReportTw cashShift={cashShiftFoundResponse.data} grossProfit={grossProfitResponse.data}  />
       </div>
     </div>
   );
