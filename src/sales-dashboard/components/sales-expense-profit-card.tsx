71e20094b3b9f5b8d523e5550111609e09007e81
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-23 22:35:01 -0500

    Add sales-expense-profit component  in the sales dashboard

---
+++ src/sales-dashboard/components/sales-expense-profit-card.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {Card, CardContent, CardHeader, CardTitle} from "@/shared/components/ui/card";
+import {Banknote} from "lucide-react";
+import React from "react";
+import {formatPrice} from "@/lib/utils";
+
+interface SalesExpenseProfitCardProps {
+  expenses: number;
+}
+
+export function SalesExpenseProfitCard({expenses}:SalesExpenseProfitCardProps) {
+  console.log(expenses);
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Ventas totales</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-green-300">
+              <Banknote size={40} color="#105d11"/>
+            </div>
+            <div className="text-2xl font-bold">S/. 55 000</div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Gastos totales</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-red-300">
+              <Banknote size={40} color="#c10d0d"/>
+            </div>
+            <div className="text-2xl font-bold">{formatPrice(expenses || 0)}</div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Utilidad</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-green-300">
+              <Banknote size={40} color="#105d11"/>
+            </div>
+            <div className="text-2xl font-bold">S/. 43 000</div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
