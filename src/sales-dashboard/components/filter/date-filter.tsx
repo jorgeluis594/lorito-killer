71be227bc4a21020ab1c4d39c80e8c1883a75040
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-23 22:36:13 -0500

    Add date filter component

---
+++ src/sales-dashboard/components/filter/date-filter.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {useState} from "react";
+import DateRangePicker from "@/shared/components/ui/date-range-picker";
+import useUpdateQueryString from "@/lib/use-update-query-string";
+import {addDays, endOfDay, startOfDay, startOfWeek, subDays} from "date-fns";
+import {useSearchParams} from "next/navigation";
+import {fromDate} from "@internationalized/date";
+
+interface DateFilterDashboardProps {
+  period: string
+}
+
+export default function DateFilterDashboard({period}:DateFilterDashboardProps) {
+  const updateRoute = useUpdateQueryString();
+  const searchParams = useSearchParams();
+  const from = searchParams.get("start");
+  const to = searchParams.get("end");
+
+  const [date, setDate] = useState<{ from: Date; to: Date } | undefined>({
+    from: from ? new Date(from) : subDays(new Date(), 20),
+    to: to ? new Date(to) : new Date(),
+  });
+
+  const onDateChange = (date: { from: Date; to: Date }) => {
+    setDate(date);
+    updateRoute({
+      start: startOfDay(date.from).toISOString(),
+      end: endOfDay(date.to).toISOString(),
+    });
+  };
+
+  return (
+    <div>
+      <DateRangePicker
+        placeholder="Rango de fechas a filtrar"
+        value={date}
+        onValueChange={onDateChange}
+      />
+    </div>
+  );
+}
