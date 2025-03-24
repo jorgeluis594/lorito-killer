9ff2426538524916193958591e22e3679fd2ae0c
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-24 14:52:50 -0500

    Add findUtility in the dasboard db repository

--- src/sales-dashboard/db_repository.ts
+++ src/sales-dashboard/db_repository.ts
@@ -1,10 +1,9 @@
 import {response} from "@/lib/types";
 import prisma from "@/lib/prisma";
 import {ExpenseAmount, ProductToSales, Sales} from "@/sales-dashboard/type";
-import {errorResponse, plus} from "@/lib/utils";
-import {log} from "@/lib/log";
-import {OrderItem} from "@/order/types";
+import { plus} from "@/lib/utils";
 import {SearchParams} from "@/document/types";
+import {GrossProfit} from "@/cash-shift/types";
 
 const findCashShift = async (
   companyId: string,
@@ -14,7 +13,15 @@ const findCashShift = async (
    return prisma().cashShift.findMany({
      where: {companyId: companyId, openedAt: { gte: startDate,lte: endDate}},
       include: {
-       orders: true,
+       orders: {
+         include: {
+           orderItems: {
+             include: {
+               product: true,
+             },
+           },
+         },
+       },
        expenses: true,
     }
    })
@@ -110,4 +117,55 @@ export const findExpenses = async ({
   }, 0);
 
   return {success:true, data: {expenseTotal: totalExpenses}}
+}
+
+export const findOrdersUtility = async ({
+  companyId,
+  startDate,
+  endDate,
+}: SearchParams): Promise<response<GrossProfit>> => {
+  const ordersToCashisftResponse = await findCashShift(companyId, startDate!, endDate!);
+  console.log(ordersToCashisftResponse)
+
+  const ordersMap = ordersToCashisftResponse.map((cashShift) => {
+    return cashShift.orders.map((order) => {
+
+      const isCancelled = order.status === "CANCELLED";
+
+      const orderTotal = order.orderItems?.reduce((total, o) => {
+        const purchaseTotal = +o.product.purchasePrice! * +o.quantity || 0;
+        const totalDifference = +o.total - purchaseTotal || 0;
+
+        total.totalPrice += purchaseTotal;
+        total.totalDifference += totalDifference;
+        console.log(total.totalPrice)
+        console.log(total.totalDifference)
+
+        return total;
+      }, {
+        totalPrice: 0,
+        totalDifference: 0
+      }) || { totalPrice: 0, totalDifference: 0 };
+
+      if (isCancelled) {
+        orderTotal.totalPrice -= orderTotal.totalPrice;
+        orderTotal.totalDifference -= orderTotal.totalDifference;
+      }
+
+      const utility = orderTotal.totalPrice - orderTotal.totalDifference;
+
+      return {
+        totalAmount: orderTotal.totalPrice,
+        totalDifference: orderTotal.totalDifference,
+        utility,
+      };
+    });
+  });
+
+  const totalUtility = ordersMap.flat(2).reduce((sum, order) => sum + order.utility, 0);
+  console.log(totalUtility)
+  return {
+    success: true,
+    data: {utility: totalUtility}
+  }
 }
