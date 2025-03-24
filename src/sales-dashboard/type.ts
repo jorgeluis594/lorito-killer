088e169f01c19294fb448ef19c7d7aa426547a3f
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-23 22:37:51 -0500

    Add Expense Amount type

--- src/sales-dashboard/type.ts
+++ src/sales-dashboard/type.ts
@@ -4,6 +4,10 @@ export type Sales = {
   finalAmount: number
 };
 
+export type ExpenseAmount = {
+  expenseTotal: number
+}
+
 export type ProductToSales = {
   productId: string,
   productName: string,
