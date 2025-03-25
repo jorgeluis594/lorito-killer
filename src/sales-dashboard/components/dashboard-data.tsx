5bae67d8f8db491d5a9ce5e40d39fd668f007e26
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-25 18:04:58 -0500

    Add dashboard data component

---
+++ src/sales-dashboard/components/dashboard-data.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import TableData from "@/sales-dashboard/components/table-data";
+import BestSeller from "@/sales-dashboard/components/best-selling-product";
+import React, {useState} from "react";
+import {SalesExpenseProfitCard} from "@/sales-dashboard/components/sales-expense-profit-card";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/shared/components/ui/select";
+import DateFilterDashboard from "@/sales-dashboard/components/filter/date-filter";
+
+interface DashboardData {
+  sales: number;
+  expenses: number;
+  utility: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+export default function DashboardData({sales, expenses, utility,startDate,endDate}:DashboardData) {
+  const [period, setPeriod] = useState<string>("")
+
+  const handleSelectChange = (value: string) => {
+    setPeriod(value);
+  };
+
+  return (
+    <>
+      <div className="flex flex-row gap-4">
+        <div>
+          <Select onValueChange={handleSelectChange}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Seleccione periodo"/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">Diario</SelectItem>
+              <SelectItem value="weekly">Semanal</SelectItem>
+              <SelectItem value="monthly">Mensual</SelectItem>
+              <SelectItem value="annual">Anual</SelectItem>
+              <SelectItem value="personalized">Personalizado</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <DateFilterDashboard/>
+        </div>
+      </div>
+      <SalesExpenseProfitCard sales={sales} expenses={expenses} utility={utility}/>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
+        <TableData period={period} sales={sales}/>
+        <BestSeller startDate={startDate} endDate={endDate}/>
+      </div>
+    </>
+  );
+}
+
