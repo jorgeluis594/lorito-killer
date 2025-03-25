cb1bab53353c22a362b5fbf69150f70c0edf702e
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-25 18:33:39 -0500

    Refactor findProductToSales Action for use with starDate and endDate

--- src/sales-dashboard/actions.ts
+++ src/sales-dashboard/actions.ts
@@ -2,15 +2,16 @@
 
 import {response} from "@/lib/types";
 import {ProductToSales, Sales} from "@/sales-dashboard/type";
-import {findProductToSales, findSales} from "@/sales-dashboard/db_repository";
+import {findProductToSales, findSales, findSalesBar} from "@/sales-dashboard/db_repository";
 import {errorResponse} from "@/lib/utils";
 import {log} from "@/lib/log";
 import {getSession} from "@/lib/auth";
+import {SearchParams} from "@/document/types";
 
 export const calculateSalesMonthly= async (startOfMonth: Date, endOfMonth: Date): Promise<response<Sales>> => {
 
   const session = await getSession();
-  const salesResponse = await findSales(session.user?.companyId!, startOfMonth, endOfMonth)
+  const salesResponse = await findSalesBar(session.user?.companyId!, startOfMonth, endOfMonth)
 
   if (!salesResponse.success) {
     log.error("sales_not_found",{salesResponse, startOfMonth, endOfMonth})
@@ -20,6 +21,7 @@ export const calculateSalesMonthly= async (startOfMonth: Date, endOfMonth: Date)
   return {success: true, data: {finalAmount: salesResponse.data.finalAmount}}
 }
 
-export const findProductToSalesAction = async (): Promise<response<ProductToSales[]>> => {
-  return findProductToSales()
+
+export const findProductToSalesAction = async (startDate: Date, endDate: Date): Promise<response<ProductToSales[]>> => {
+  return findProductToSales(startDate!,endDate!)
 }
