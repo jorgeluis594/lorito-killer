e5d38240e21482a23d39d00fa490aae78bc17b60
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-25 18:05:44 -0500

    Refactor page for use dashboard data component

--- src/app/[subdomain]/dashboard/(dashboard)/page.tsx
+++ src/app/[subdomain]/dashboard/(dashboard)/page.tsx
@@ -1,15 +1,12 @@
 import {ScrollArea} from "@/shared/components/ui/scroll-area";
-import TableData from "@/sales-dashboard/components/table-data";
-import BestSeller from "@/sales-dashboard/components/best-selling-product";
 import React from "react";
-import DateFilterSelect from "@/sales-dashboard/components/date-filter-select";
-import {SalesExpenseProfitCard} from "@/sales-dashboard/components/sales-expense-profit-card";
 import {response} from "@/lib/types";
 import {SearchParams} from "@/document/types";
 import {getSession} from "@/lib/auth";
 import {errorResponse} from "@/lib/utils";
 import SignOutRedirection from "@/shared/components/sign-out-redirection";
-import {findExpenses, findOrdersUtility, findSales} from "@/sales-dashboard/db_repository";
+import {findExpenses, findOrdersUtility, findProductToSales, findSales} from "@/sales-dashboard/db_repository";
+import DashboardData from "@/sales-dashboard/components/dashboard-data";
 
 type ParamsProps = {
   searchParams: {
@@ -39,16 +36,16 @@ const getSearchParams = async ({
   return {success: true, data: params};
 };
 
-async function SalesExpenseProfitCardWithSuspense({searchParams}: ParamsProps) {
-  const dateQuery = await getSearchParams({searchParams});
-  if (!dateQuery.success) {
+async function DashboardDataWithSuspense({searchParams}: ParamsProps) {
+  const dataQuery = await getSearchParams({searchParams});
+  if (!dataQuery.success) {
     return <SignOutRedirection/>;
   }
 
   const [expensesResponse, totalSalesResponse, utilityResponse] = await Promise.all([
-    findExpenses(dateQuery.data),
-    findSales(dateQuery.data),
-    findOrdersUtility(dateQuery.data)
+    findExpenses(dataQuery.data),
+    findSales(dataQuery.data),
+    findOrdersUtility(dataQuery.data),
   ]);
 
   if (!expensesResponse.success || !totalSalesResponse.success || !utilityResponse.success) {
@@ -56,7 +53,13 @@ async function SalesExpenseProfitCardWithSuspense({searchParams}: ParamsProps) {
   }
 
   return (
-    <SalesExpenseProfitCard sales={totalSalesResponse.data.finalAmount} expenses={expensesResponse.data.expenseTotal} utility={utilityResponse.data.utility} />
+    <DashboardData
+      sales={totalSalesResponse.data.finalAmount}
+      expenses={expensesResponse.data.expenseTotal}
+      utility={utilityResponse.data.utility}
+      startDate={dataQuery.data.startDate!}
+      endDate={dataQuery.data.endDate!}
+    />
   );
 }
 
@@ -67,12 +70,7 @@ export default function page({searchParams}: ParamsProps) {
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-4xl font-bold tracking-tight text-gray-800">Datos</h2>
         </div>
-        <DateFilterSelect/>
-        <SalesExpenseProfitCardWithSuspense searchParams={searchParams}/>
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
-          <TableData/>
-          <BestSeller/>
-        </div>
+        <DashboardDataWithSuspense searchParams={searchParams} />
       </div>
     </ScrollArea>
   );
