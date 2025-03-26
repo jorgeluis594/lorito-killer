dfcd6c62a577d6b065d29cc824ccbe12bde79ce2
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-26 01:22:13 -0500

    Add sales daily and sales weekly in the dashboard action

--- src/sales-dashboard/actions.ts
+++ src/sales-dashboard/actions.ts
@@ -1,17 +1,46 @@
 "use server";
 
 import {response} from "@/lib/types";
-import {ProductToSales, Sales} from "@/sales-dashboard/type";
-import {findProductToSales, findSales, findSalesBar} from "@/sales-dashboard/db_repository";
+import {ProductToSales, Sales, SalesWeekly} from "@/sales-dashboard/type";
+import {
+  findProductToSales, findSalesDaily,
+  findSalesMonthly,
+  findSalesWeekly
+} from "@/sales-dashboard/db_repository";
 import {errorResponse} from "@/lib/utils";
 import {log} from "@/lib/log";
 import {getSession} from "@/lib/auth";
-import {SearchParams} from "@/document/types";
+
+export const calculateSalesDaily= async (startOfDay: Date, endOfDay: Date): Promise<response<SalesWeekly>> => {
+
+  const session = await getSession();
+  const salesResponse = await findSalesDaily(session.user?.companyId!, startOfDay, endOfDay)
+
+  if (!salesResponse.success) {
+    log.error("sales_not_found",{salesResponse, startOfDay, endOfDay})
+    return errorResponse("Sales not found")
+  }
+
+  return {success: true, data: {salesByDay: salesResponse.data.salesByDay,}}
+}
+
+export const calculateSalesWeekly= async (startDate: Date, endDate: Date): Promise<response<SalesWeekly>> => {
+
+  const session = await getSession();
+  const salesResponse = await findSalesWeekly(session.user?.companyId!, startDate, endDate)
+
+  if (!salesResponse.success) {
+    log.error("sales_not_found",{salesResponse, startDate, endDate})
+    return errorResponse("Sales not found")
+  }
+
+  return {success: true, data: {salesByDay: salesResponse.data.salesByDay,}}
+}
 
 export const calculateSalesMonthly= async (startOfMonth: Date, endOfMonth: Date): Promise<response<Sales>> => {
 
   const session = await getSession();
-  const salesResponse = await findSalesBar(session.user?.companyId!, startOfMonth, endOfMonth)
+  const salesResponse = await findSalesMonthly(session.user?.companyId!, startOfMonth, endOfMonth)
 
   if (!salesResponse.success) {
     log.error("sales_not_found",{salesResponse, startOfMonth, endOfMonth})
@@ -20,7 +49,6 @@ export const calculateSalesMonthly= async (startOfMonth: Date, endOfMonth: Date)
 
   return {success: true, data: {finalAmount: salesResponse.data.finalAmount}}
 }
-
 
 export const findProductToSalesAction = async (startDate: Date, endDate: Date): Promise<response<ProductToSales[]>> => {
   return findProductToSales(startDate!,endDate!)
