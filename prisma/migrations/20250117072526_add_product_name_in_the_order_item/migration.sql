b0c0448da916796ff2807df3af5b184b81b1cd9b
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-03-26 22:02:32 -0500

    Refactor to split orderItem migration logic

--- prisma/migrations/20250117072526_add_product_name_in_the_order_item/migration.sql
+++ prisma/migrations/20250117072526_add_product_name_in_the_order_item/migration.sql
@@ -6,8 +6,3 @@
 */
 -- AlterTable
 ALTER TABLE "OrderItem" ADD COLUMN "productName" TEXT;
-UPDATE "OrderItem" oi
-SET "productName" = p.name
-FROM "Product" p
-WHERE oi."productId" = p.id;
-ALTER TABLE "OrderItem" ALTER COLUMN "productName" SET NOT NULL;
