46862906383a119ff0d12ebdfaa28649d689c533
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-04-24 06:51:54 -0500

    Use card responsive component

--- src/sale_report/components/table/client.tsx
+++ src/sale_report/components/table/client.tsx
@@ -38,6 +38,7 @@ import { Skeleton } from "@/shared/components/ui/skeleton";
 import useUpdateQueryString from "@/lib/use-update-query-string";
 import { Input } from "@/shared/components/ui/input";
 import { SearchIcon } from "lucide-react";
+import CardResponsive from "@/shared/components/ui/card-responsive";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -193,37 +194,7 @@ export default function DataTable<TData, TValue>({
         </ScrollArea>
       </div>
 
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:hidden">
-        {table.getRowModel().rows.map((row) => (
-          <div
-            key={row.id}
-            className="bg-white space-y-3 p-4 rounded-lg shadow relative w-full max-w-sm"
-          >
-            {row.getVisibleCells().map((cell, index, arr) => {
-              const isLast = index === arr.length - 1;
-
-              return (
-                <div
-                  key={cell.id}
-                  className={`py-2 px-4 space-y-1 ${isLast ? 'absolute top-4 right-4 text-right w-fit max-w-[90%]' : ''}`}
-                >
-                  {!isLast && (
-                    <p className="text-xs text-gray-500 font-medium">
-                      {typeof cell.column.columnDef.header === 'string'
-                        ? cell.column.columnDef.header
-                        : ''}
-                    </p>
-                  )}
-                  <p className="text-sm text-gray-900 font-semibold">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </p>
-                </div>
-              );
-            })}
-          </div>
-        ))}
-      </div>
+      <CardResponsive table={table}/>
 
       <div className="flex flex-col items-center justify-end gap-2 space-x-2 py-4 sm:flex-row">
         <div className="flex w-full items-center justify-between">
