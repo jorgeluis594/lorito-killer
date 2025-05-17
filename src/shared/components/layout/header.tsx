3b7ee9b7f1cad29c1518207451354371449f9057
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-05-17 12:17:53 -0500

    Fix sidebar hidden

--- src/shared/components/layout/header.tsx
+++ src/shared/components/layout/header.tsx
@@ -8,7 +8,9 @@ export default function Header() {
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className="flex items-center">
-          <MobileSidebar/>
+          <div className={cn("block lg:!hidden")}>
+            <MobileSidebar/>
+          </div>
           <div className="flex relative w-[150px] h-[56px] rounded-md overflow-hidden ml-2">
             <LogoImage/>
           </div>
